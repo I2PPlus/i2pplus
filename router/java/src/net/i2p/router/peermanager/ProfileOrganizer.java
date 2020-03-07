1bfff1ad621e010b54ee07b09ec18f22117fe362
dr|z3d <5555808-i2pplus@users.noreply.gitlab.com>
2020-03-07 18:01:14 +0000

    Restore mainline defaults for ENOUGH_PROFILES (600 vs 1200).

--- router/java/src/net/i2p/router/peermanager/ProfileOrganizer.java
+++ router/java/src/net/i2p/router/peermanager/ProfileOrganizer.java
@@ -829,8 +829,7 @@ public class ProfileOrganizer {
 //    private static final long MAX_EXPIRE_TIME = 6*60*60*1000;
     private static final long MAX_EXPIRE_TIME = 8*60*60*1000;
     private static final long ADJUST_EXPIRE_TIME = 60*1000;
-//    private static final int ENOUGH_PROFILES = 600;
-    private static final int ENOUGH_PROFILES = 1200;
+    private static final int ENOUGH_PROFILES = 600;
     private long _currentExpireTime = MAX_EXPIRE_TIME;
 
     /**
