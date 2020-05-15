a8323e774638d72becb94e727514e483622d2eb9
zzz <zzz@mail.i2p>
2020-05-15 21:31:37 +0000

    i2ptunnel: Remove javascript from proxy error pages Add security headers Fix typo on resetp-header

--- apps/i2ptunnel/java/src/net/i2p/i2ptunnel/I2PTunnelHTTPClientBase.java
+++ apps/i2ptunnel/java/src/net/i2p/i2ptunnel/I2PTunnelHTTPClientBase.java
@@ -920,9 +920,10 @@ public abstract class I2PTunnelHTTPClientBase extends I2PTunnelClientBase implem
         if (outs == null)
             return;
         Writer out = new BufferedWriter(new OutputStreamWriter(outs, "UTF-8"));
-        out.write(errMessage);
         if (targetRequest != null) {
             String uri = DataHelper.escapeHTML(targetRequest);
+            errMessage = errMessage.replace("<a href=\"\">", "<a href=\"" + uri + "\">");
+            out.write(errMessage);
             out.write("<a href=\"");
             out.write(uri);
             out.write("\">");
@@ -987,6 +988,8 @@ public abstract class I2PTunnelHTTPClientBase extends I2PTunnelClientBase implem
                     out.write("</div>\n");
                 }
             }
+        } else {
+            out.write(errMessage);
         }
         out.write("</div>\n");
         writeFooter(out);
