eca8005667a1fb3949c0d686cfe94735ad7e35c8
dr|z3d <z3d@i2pmail.org>
2020-08-18 16:47:00 +0000

    I2PSnark: tweak caching

--- apps/i2psnark/java/src/org/klomp/snark/web/I2PSnarkServlet.java
+++ apps/i2psnark/java/src/org/klomp/snark/web/I2PSnarkServlet.java
@@ -469,7 +469,7 @@ public class I2PSnarkServlet extends BasicServlet {
         resp.setContentType("text/html; charset=UTF-8");
         // "no-store, max-age=0" forces all our images to be reloaded on ajax refresh
         // add 'private' header to avoid being cached in anything other than browser
-        resp.setHeader("Cache-Control", "no-cache, private, max-age=2628000");
+        resp.setHeader("Cache-Control", "no-cache, private, max-age=2628000, stale-while-revalidate=86400");
 //        resp.setHeader("Cache-Control", "max-age=86400, private, must-revalidate"); // theme change issues with XHR
         resp.setHeader("Content-Security-Policy", "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'nonce-" + cspNonce + "'; form-action 'self'; frame-ancestors 'self'; object-src 'none'; media-src '" + (allowMedia ? "self" : "none") + "'");
         // This header is broken, only takes a date. Not needed with 'max-age'.
