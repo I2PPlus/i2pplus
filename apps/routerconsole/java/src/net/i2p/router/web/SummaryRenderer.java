44c3c52ae2e64652a8e82c7d63ce55239b971880
dr|z3d <z3d@i2pmail.org>
2020-09-18 11:22:59 +0000

    Graphing: when subgrid hidden, tone dark/midnight gridlines per theme

--- apps/routerconsole/java/src/net/i2p/router/web/SummaryRenderer.java
+++ apps/routerconsole/java/src/net/i2p/router/web/SummaryRenderer.java
@@ -72,7 +72,9 @@ class SummaryRenderer {
     private static final Color FONT_COLOR_DARK = new Color(244, 244, 190);
     private static final Color FONT_COLOR_MIDNIGHT = new Color(201, 206, 255);
     private static final Color AXIS_COLOR_DARK = new Color(244, 244, 190, 200);
+    private static final Color AXIS_COLOR_DARK2 = new Color(244, 244, 190, 72);
     private static final Color AXIS_COLOR_MIDNIGHT = new Color(201, 206, 255, 200);
+    private static final Color AXIS_COLOR_MIDNIGHT2 = new Color(201, 206, 255, 72);
     //private static final Color FRAME_COLOR = new Color(51, 51, 63);
     private static final Color FRAME_COLOR = new Color(0, 0, 0, 0);
     //private static final Color FRAME_COLOR_DARK = new Color(16, 16, 16);
@@ -226,8 +228,10 @@ class SummaryRenderer {
 
             if (width < 400 || height < 200 || periodCount < 120) {
                 def.setColor(ElementsNames.grid, GRID_COLOR_HIDDEN);
-                if (theme.equals("midnight") || theme.equals("dark"))
-                  def.setColor(ElementsNames.mgrid, GRID_COLOR_DARK);
+                if (theme.equals("midnight"))
+                  def.setColor(ElementsNames.mgrid, AXIS_COLOR_MIDNIGHT2);
+                else if (theme.equals("dark"))
+                  def.setColor(ElementsNames.mgrid, AXIS_COLOR_DARK2);
                 else
                   def.setColor(ElementsNames.mgrid, GRID_COLOR);
             }
@@ -390,7 +394,8 @@ class SummaryRenderer {
                 name = name.replace("OutBps", "Outbound B/s");
             // heuristic to set K=1024
             //if ((name.startsWith("bw.") || name.indexOf("Size") >= 0 || name.indexOf("Bps") >= 0 || name.indexOf("memory") >= 0)
-            if ((name.indexOf("Size") >= 0 || name.indexOf("memory") >= 0)
+
+            if ((name.indexOf("Size") >= 0 || name.indexOf("memory") >= 0) || name.contains("B/s") || name.contains("Bandwidth")
                 && !showEvents)
                 def.setBase(1024);
             if (titleOverride != null) {
