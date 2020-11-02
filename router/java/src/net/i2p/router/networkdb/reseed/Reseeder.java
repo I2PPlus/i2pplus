39c912eec91840a485ac84cf838f3b2fbbce0a83
zzz <zzz@mail.i2p>
2020-11-02 22:19:49 +0000

    Remove reseed SSL cert, now using a CA
    (cherry picked from commit c18dbe974a4abf1e3e88a3062d671d7d06b777fa)

--- router/java/src/net/i2p/router/networkdb/reseed/Reseeder.java
+++ router/java/src/net/i2p/router/networkdb/reseed/Reseeder.java
@@ -120,7 +120,7 @@ public class Reseeder {
         "https://reseed.i2pgit.org/"                + ',' +   // hankhill19580_at_gmail.com.crt    CA                                Java 8+
         "https://i2p.novg.net/"                     + ',' +   // igor_at_novg.net.crt              CA                                Java 8+
         "https://i2pseed.creativecowpat.net:8443/"  + ',' +   // creativecowpat_at_mail.i2p.crt    i2pseed.creativecowpat.net.crt    Java 7+
-        "https://reseed.onion.im/"                  + ',' +   // lazygravy_at_mail.i2p             reseed.onion.im.crt               Java 8+
+        "https://reseed.onion.im/"                  + ',' +   // lazygravy_at_mail.i2p             CA                                Java 8+
         "https://reseed.memcpy.io/"                 + ',' +   // hottuna_at_mail.i2p.crt           CA                                SNI required
         "https://reseed.i2p-projekt.de/";                     // echelon_at_mail.i2p.crt           echelon.reseed2017.crt            Java 8+
 
