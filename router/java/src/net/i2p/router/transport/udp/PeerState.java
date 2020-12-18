62163f17378599fd72fe92ed5a9cbbab5c0f321e
zzz <zzz@i2pmail.org>
2020-12-18 11:52:18 -0500

    SSU: Account for packet overhead in window calculations
    (cherry picked from commit b9094307257fa9881f089d9448afcda856c0bf99)

--- router/java/src/net/i2p/router/transport/udp/PeerState.java
+++ router/java/src/net/i2p/router/transport/udp/PeerState.java
@@ -627,7 +627,7 @@ public class PeerState {
                 _context.statManager().addRateData("udp.rejectConcurrentActive", _outboundMessages.size(), _consecutiveRejections);
                 return false;
         }
-        if (_sendWindowBytesRemaining <= 0)
+        if (_sendWindowBytesRemaining <= fragmentOverhead())
             return false;
 
         int size = state.getSendSize(_sendWindowBytesRemaining);
@@ -1663,6 +1663,18 @@ public class PeerState {
                MIN_ACK_SIZE;
     }
 
+    /**
+     *  Packet overhead plus room for acks
+     *  @return 87 (IPv4), 107 (IPv6)
+     *  @since 0.9.49
+     */
+    int fragmentOverhead() {
+        // 46 + 20 + 8 + 13 = 74 + 13 = 87 (IPv4)
+        // 46 + 40 + 8 + 13 = 94 + 13 = 107 (IPv6)
+        return (_remoteIP.length == 4 ? PacketBuilder.MIN_DATA_PACKET_OVERHEAD : PacketBuilder.MIN_IPV6_DATA_PACKET_OVERHEAD) +
+               MIN_ACK_SIZE;
+    }
+    
     /**
      *  Locks this.
      */
