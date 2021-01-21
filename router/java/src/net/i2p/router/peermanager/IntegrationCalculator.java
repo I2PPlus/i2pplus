341d7917eef9a716c219b75eaae00f88208f0e2c
dr|z3d <z3d@i2pmail.org>
2021-01-21 07:23:47 +0000

    router: integrationcalculator tweaks

--- router/java/src/net/i2p/router/peermanager/IntegrationCalculator.java
+++ router/java/src/net/i2p/router/peermanager/IntegrationCalculator.java
@@ -11,17 +11,13 @@ class IntegrationCalculator {
         long val = 0;
         if (profile.getIsExpandedDB()) {
             // give more weight to recent counts
-            val =  profile.getDbIntroduction().getRate(24*60*60*1000l).getLastEventCount() / 2;
+            val =  profile.getDbIntroduction().getRate(24*60*60*1000l).getLastEventCount();
             val += profile.getDbIntroduction().getRate(24*60*60*1000l).getCurrentEventCount() * 2;
-            val += 24 * profile.getDbIntroduction().getRate(60*60*1000l).getLastEventCount() / 2;
+            val += 24 * profile.getDbIntroduction().getRate(60*60*1000l).getLastEventCount();
             val += 24 * profile.getDbIntroduction().getRate(60*60*1000l).getCurrentEventCount() * 2;
-            val += 6 * 24 * profile.getDbIntroduction().getRate(10*60*1000l).getLastEventCount() / 2;
+            val += 6 * 24 * profile.getDbIntroduction().getRate(10*60*1000l).getLastEventCount();
             val += 6 * 24 * profile.getDbIntroduction().getRate(10*60*1000l).getCurrentEventCount() * 2;
-
             val /= 24;
-
-            if (val > 0 && val < 1)
-                val *= 10;
             if (val > 0 && val < 1)
                 val = 1;
         }
