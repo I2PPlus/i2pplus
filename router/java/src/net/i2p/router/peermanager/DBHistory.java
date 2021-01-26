743985eaa487430de5ec308680103e5b5c4e60f2
dr|z3d <z3d@i2pmail.org>
2021-01-26 15:33:13 +0000

    profiles: streamline invalidReplyRate stat collection

--- router/java/src/net/i2p/router/peermanager/DBHistory.java
+++ router/java/src/net/i2p/router/peermanager/DBHistory.java
@@ -325,7 +325,7 @@ public class DBHistory {
             _failedLookupRate = new RateStat("dbHistory.failedLookupRate", "How often peer responds to a lookup", statGroup, new long[] { 10*60*1000l, 60*60*1000l, 24*60*60*1000l });
         if (_invalidReplyRate == null)
 //            _invalidReplyRate = new RateStat("dbHistory.invalidReplyRate", "How often peer gives us an invalid peer", statGroup, new long[] { 30*60*1000l });
-            _invalidReplyRate = new RateStat("dbHistory.invalidReplyRate", "How often peer gives us an invalid peer", statGroup, new long[] { 10*60*1000l, 60*60*1000l, 24*60*60*1000l });
+            _invalidReplyRate = new RateStat("dbHistory.invalidReplyRate", "How often peer gives us an invalid peer", statGroup, new long[] { 60*60*1000l });
     }
 
     private final static long getLong(Properties props, String key) {
