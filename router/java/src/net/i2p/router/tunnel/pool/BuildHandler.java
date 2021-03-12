2aac12b215ea21b60af980d96083c1a64110f033
dr|z3d <z3d@i2pmail.org>
2021-03-12 04:17:21 +0000

    Router: tweak min/max lookup limits for BuildHandler

--- router/java/src/net/i2p/router/tunnel/pool/BuildHandler.java
+++ router/java/src/net/i2p/router/tunnel/pool/BuildHandler.java
@@ -76,9 +76,9 @@ class BuildHandler implements Runnable {
 
     /** TODO these may be too high, review and adjust */
 //    private static final int MIN_QUEUE = 18;
-    private static final int MIN_QUEUE = 12;
+    private static final int MIN_QUEUE = 32;
 //    private static final int MAX_QUEUE = 192;
-    private static final int MAX_QUEUE = 128;
+    private static final int MAX_QUEUE = 256;
 
 //    private static final int NEXT_HOP_LOOKUP_TIMEOUT = 15*1000;
     private static final int NEXT_HOP_LOOKUP_TIMEOUT = 10*1000;
@@ -86,9 +86,9 @@ class BuildHandler implements Runnable {
 
     /** limits on concurrent next-hop RI lookup */
 //    private static final int MIN_LOOKUP_LIMIT = 10;
-    private static final int MIN_LOOKUP_LIMIT = 20;
+    private static final int MIN_LOOKUP_LIMIT = 16;
 //    private static final int MAX_LOOKUP_LIMIT = 100;
-    private static final int MAX_LOOKUP_LIMIT = 64;
+    private static final int MAX_LOOKUP_LIMIT = 160;
     /** limit lookups to this % of current participating tunnels */
 //    private static final int PERCENT_LOOKUP_LIMIT = 3;
     private static final int PERCENT_LOOKUP_LIMIT = 5;
