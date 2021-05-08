9ec6ea2805abe201d8afd9c355615a85411c69d6
dr|z3d <z3d@i2pmail.org>
2021-05-08 06:41:28 +0000

    Router: tweak max build handlers

--- router/java/src/net/i2p/router/tunnel/pool/BuildExecutor.java
+++ router/java/src/net/i2p/router/tunnel/pool/BuildExecutor.java
@@ -48,7 +48,7 @@ class BuildExecutor implements Runnable {
     private volatile boolean _isRunning;
     private boolean _repoll;
 //    private static final int MAX_CONCURRENT_BUILDS = 13;
-    private static final int MAX_CONCURRENT_BUILDS = SystemVersion.getCores() * 4;
+    private static final int MAX_CONCURRENT_BUILDS = Math.max(SystemVersion.getCores() * 3, 16);
     /** accept replies up to a minute after we gave up on them */
     private static final long GRACE_PERIOD = 60*1000;
 
