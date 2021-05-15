966ab4467a91675bfe431e82050eb815b65606d5
dr|z3d <z3d@i2pmail.org>
2021-05-15 10:35:12 +0000

    Router: increase priority blocking queue size and remove backlog buffer @ >=1024MB;

--- router/java/src/net/i2p/router/util/PriBlockingQueue.java
+++ router/java/src/net/i2p/router/util/PriBlockingQueue.java
@@ -30,8 +30,8 @@ public class PriBlockingQueue<E extends PQEntry> extends PriorityBlockingQueue<E
 
     private final String STAT_FULL;
     protected static final long[] RATES = {60*1000, 10*60*1000, 60*60*1000};
-    protected static final int BACKLOG_SIZE = SystemVersion.getMaxMemory() < 1024*1024*1024 ? 256 : 128;
-    protected static final int MAX_SIZE = SystemVersion.getMaxMemory() < 1024*1024*1024 ? 512 : 1024;
+    protected static final int BACKLOG_SIZE = SystemVersion.getMaxMemory() < 1024*1024*1024 ? 256 : 0;
+    protected static final int MAX_SIZE = SystemVersion.getMaxMemory() < 1024*1024*1024 ? 512 : 16384;
 
     /**
      *  Bounded queue with a hardcoded failsafe max size,
