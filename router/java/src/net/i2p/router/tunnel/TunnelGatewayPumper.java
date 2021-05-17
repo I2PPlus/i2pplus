ad65fb3ae6fca32a3d05c9b05b6fbadd7fb97993
dr|z3d <z3d@i2pmail.org>
2021-05-17 20:13:24 +0000

    Router: increase requeue time in TunnelGatewayPumper

--- router/java/src/net/i2p/router/tunnel/TunnelGatewayPumper.java
+++ router/java/src/net/i2p/router/tunnel/TunnelGatewayPumper.java
@@ -30,7 +30,7 @@ class TunnelGatewayPumper implements Runnable {
     private static final int MIN_PUMPERS = 1;
 //    private static final int MAX_PUMPERS = 4;
     private static final int MAX_PUMPERS = (SystemVersion.isSlow() || SystemVersion.getCores() <= 4 ||
-                                            SystemVersion.getMaxMemory() < 512*1024*1024) ? 3 : Math.max(SystemVersion.getCores() / 2, 6);
+                                            SystemVersion.getMaxMemory() < 512*1024*1024) ? 4 : Math.max(SystemVersion.getCores() - 2, 6);
     private final int _pumpers;
 
     /**
@@ -38,7 +38,7 @@ class TunnelGatewayPumper implements Runnable {
      *  See additional comments in PTG.
      */
 //    private static final long REQUEUE_TIME = 50;
-    private static final long REQUEUE_TIME = 40;
+    private static final long REQUEUE_TIME = 100;
 
     /** Creates a new instance of TunnelGatewayPumper */
     public TunnelGatewayPumper(RouterContext ctx) {
