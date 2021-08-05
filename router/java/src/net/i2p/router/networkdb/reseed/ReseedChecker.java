6977ba234024004f7791df1af18c976c720c55de
dr|z3d <z3d@i2pmail.org>
2021-08-05 15:06:40 +0000

    Router: reduce minimum # of known routers before auto-reseed

--- router/java/src/net/i2p/router/networkdb/reseed/ReseedChecker.java
+++ router/java/src/net/i2p/router/networkdb/reseed/ReseedChecker.java
@@ -35,7 +35,7 @@ public class ReseedChecker {
     private volatile boolean _alreadyRun;
 
     //public static final int MINIMUM = 50;
-    public static final int MINIMUM = 400; // minimum number of router infos before automatic reseed attempted
+    public static final int MINIMUM = 80; // minimum number of router infos before automatic reseed attempted
     //private static final long STATUS_CLEAN_TIME = 20*60*1000;
     private static final long STATUS_CLEAN_TIME = 3*60*1000; // sidebar notification persistence
     // if down this long, reseed at startup
