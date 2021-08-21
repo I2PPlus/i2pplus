2661be3299559fa48d66bf3292e5fab405420c00
dr|z3d <z3d@i2pmail.org>
2021-08-21 06:08:07 +0000

    Router: search harder for routers during first 1/2h, and uninteresting peers if < 2000 known peers or 1000 RIs on disk

--- router/java/src/net/i2p/router/networkdb/kademlia/IterativeSearchJob.java
+++ router/java/src/net/i2p/router/networkdb/kademlia/IterativeSearchJob.java
@@ -149,7 +149,9 @@ public class IterativeSearchJob extends FloodSearchJob {
                               Job onFind, Job onFailed, int timeoutMs, boolean isLease, Hash fromLocalDest) {
         super(ctx, facade, key, onFind, onFailed, timeoutMs, isLease);
         RouterInfo ri = _facade.lookupRouterInfoLocally(getContext().routerHash());
+        boolean isHidden =  ctx.router().isHidden();
         int known = ctx.netDb().getKnownRouters();
+        int netDbDiskCount = _facade.getAllRouters().size();
         int totalSearchLimit = (facade.floodfillEnabled() && ctx.router().getUptime() > 30*60*1000) ?
                                 TOTAL_SEARCH_LIMIT_WHEN_FF : TOTAL_SEARCH_LIMIT;
 
@@ -162,8 +164,10 @@ public class IterativeSearchJob extends FloodSearchJob {
             String v = ri.getVersion();
             boolean uninteresting = ri.getCapabilities().indexOf(Router.CAPABILITY_UNREACHABLE) >= 0 ||
                                     ri.getCapabilities().indexOf(Router.CAPABILITY_BW12) >= 0 ||
-                                    ri.getCapabilities().indexOf(Router.CAPABILITY_BW32) >= 0 || VersionComparator.comp(v, MIN_VERSION) < 0;
-
+                                    ri.getCapabilities().indexOf(Router.CAPABILITY_BW32) >= 0 ||
+                                    VersionComparator.comp(v, MIN_VERSION) < 0 &&
+                                    ctx.netDb().getKnownRouters() > 2000 && netDbDiskCount > 1000 &&
+                                    ctx.router().getUptime() > 60*60*1000 && !isHidden;
             if (uninteresting) {
                 _timeoutMs = Math.min(timeoutMs / 2, MAX_SEARCH_TIME / 2);
                 totalSearchLimit -= 2;
@@ -179,7 +183,7 @@ public class IterativeSearchJob extends FloodSearchJob {
         _totalSearchLimit = ctx.getProperty("netdb.searchLimit", totalSearchLimit);
         _ipSet = new MaskedIPSet(2 * (_totalSearchLimit + EXTRA_PEERS));
         _singleSearchTime = ctx.getProperty("netdb.singleSearchTime", SINGLE_SEARCH_TIME);
-        _maxConcurrent = ctx.getProperty("netdb.maxConcurrent", MAX_CONCURRENT);
+        _maxConcurrent = ctx.router().getUptime() > 30*60*1000 ? ctx.getProperty("netdb.maxConcurrent", MAX_CONCURRENT) : 2;
         _unheardFrom = new HashSet<Hash>(CONCURRENT_SEARCHES);
         _failedPeers = new HashSet<Hash>(_totalSearchLimit);
         _skippedPeers = new HashSet<Hash>(4);
