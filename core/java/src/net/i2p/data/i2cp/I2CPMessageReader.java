09777db919677f994ed5a56e9b4db1e6ab27010b
dr|z3d <z3d@i2pmail.org>
2021-09-10 19:00:23 +0000

    Syntax fixes post-upstream merge

--- core/java/src/net/i2p/data/i2cp/I2CPMessageReader.java
+++ core/java/src/net/i2p/data/i2cp/I2CPMessageReader.java
@@ -190,12 +190,13 @@ public class I2CPMessageReader {
                         _listener.readError(I2CPMessageReader.this, ime);
                         cancelRunner();
                     } catch (IOException ioe) {
-                        _log.warn("IO Error handling message \n* Error: " + ioe.getMessage());
-                        _listener.readError(I2CPMessageReader.this, ioe);
+                        if (_stayAlive) {
+                            _log.warn("IO Error handling message \n* Error: " + ioe.getMessage());
+                            _listener.readError(I2CPMessageReader.this, ioe);
                         } // else triggered by cancelRunner(), ignore SocketException
                         _listener.disconnected(I2CPMessageReader.this);
                         if (_stayAlive) {
-                        cancelRunner();
+                            cancelRunner();
                         }
                     } catch (OutOfMemoryError oom) {
                         // ooms seen here... maybe log and keep going?
