b7c54caf369334f77c7f0962d85786468c4d2ddf
zzz <zzz@i2pmail.org>
2021-12-28 08:56:47 -0500

    NetDB: Fix usage of dbResponseTime stat
    Actually update the stat for stores in dbStoreSent();
    we are generally storing to different ffs than lookups, so we need the
    stat for stores as well, since we use it as the timeout in StoreJob.
    
    Change from 1-day to 1-hour stat.
    Switch to avgOrLifetimeAvg() so the rate is always valid.
    Reduce max time used for timeout.
    This allows more peers to be tried before total timeout
    Previously, the per-peer timeout was almost always the max.
    Make sendStore() package private.
    Javadocs and cleanups.
    
    (cherry picked from commit bef729463d5d5e83f4c7d24c47859eecc506735f)

--- router/java/src/net/i2p/router/networkdb/kademlia/KademliaNetworkDatabaseFacade.java
+++ router/java/src/net/i2p/router/networkdb/kademlia/KademliaNetworkDatabaseFacade.java
@@ -873,7 +873,7 @@ public abstract class KademliaNetworkDatabaseFacade extends NetworkDatabaseFacad
                 return;
         }
 
-        RepublishLeaseSetJob j = null;
+        RepublishLeaseSetJob j;
         synchronized (_publishingLeaseSets) {
             j = _publishingLeaseSets.get(h);
             if (j == null) {
@@ -1611,7 +1611,7 @@ public abstract class KademliaNetworkDatabaseFacade extends NetworkDatabaseFacad
         PeerProfile prof = _context.profileOrganizer().getProfile(peer);
         double responseTime = MAX_PER_PEER_TIMEOUT;
         if (prof != null && prof.getIsExpandedDB()) {
-            responseTime = prof.getDbResponseTime().getRate(24*60*60*1000l).getAverageValue();
+            responseTime = prof.getDbResponseTime().getRate(60*60*1000L).getAvgOrLifetimeAvg();
             // if 0 then there is no data, set to max.
             if (responseTime <= 0 || responseTime > MAX_PER_PEER_TIMEOUT)
                 responseTime = MAX_PER_PEER_TIMEOUT;
@@ -1621,8 +1621,17 @@ public abstract class KademliaNetworkDatabaseFacade extends NetworkDatabaseFacad
         return TIMEOUT_MULTIPLIER * (int)responseTime;  // give it up to 3x the average response time
     }
 
-    /** unused (overridden in FNDF) */
-    public abstract void sendStore(Hash key, DatabaseEntry ds, Job onSuccess, Job onFailure, long sendTimeout, Set<Hash> toIgnore);
+    /**
+     * See implementation in FNDF
+     *
+     * @param key the DatabaseEntry hash
+     * @param onSuccess may be null, always called if we are ff and ds is an RI
+     * @param onFailure may be null, ignored if we are ff and ds is an RI
+     * @param sendTimeout ignored if we are ff and ds is an RI
+     * @param toIgnore may be null, if non-null, all attempted and skipped targets will be added as of 0.9.53,
+     *                 unused if we are ff and ds is an RI
+     */
+    abstract void sendStore(Hash key, DatabaseEntry ds, Job onSuccess, Job onFailure, long sendTimeout, Set<Hash> toIgnore);
 
     /**
      *  Increment in the negative lookup cache
