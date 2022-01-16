69d820ff3805cedb2d9b14455bcef9b46b78ca82
dr|z3d <z3d@i2pmail.org>
2022-01-16 03:15:50 +0000

    Graphs: show date as vertically oriented credit, not legend, if legend enabled

--- apps/routerconsole/java/src/net/i2p/router/web/SummaryRenderer.java
+++ apps/routerconsole/java/src/net/i2p/router/web/SummaryRenderer.java
@@ -554,10 +554,15 @@ class SummaryRenderer {
                         }
                     }
                 }
-                def.comment(sdf.format(new Date(start)) + " — " + sdf.format(new Date(end)) + " UTC\\r");
-            }
-
-            if (!showCredit) {
+                if (height >= 90) {
+                    def.setShowSignature(true);
+                    def.setSignature(sdf.format(new Date(start)) + " - " + sdf.format(new Date(end)) + " UTC");
+                } else {
+                    def.setShowSignature(true);
+                    def.setSignature(sdf.format(new Date(end)) + " UTC");
+                }
+//                def.comment(sdf.format(new Date(start)) + " — " + sdf.format(new Date(end)) + " UTC\\r");
+            } else if (hideLegend && !showCredit) {
                 def.setShowSignature(false);
             } else if (hideLegend) {
                 if (height > 65)
@@ -565,6 +570,7 @@ class SummaryRenderer {
                 else
                     def.setSignature(sdf.format(new Date(end)) + " UTC");
             }
+
             /*
             // these four lines set up a graph plotting both values and events on the same chart
             // (but with the same coordinates, so the values may look pretty skewed)
