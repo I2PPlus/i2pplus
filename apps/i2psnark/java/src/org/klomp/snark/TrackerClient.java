3645c40826cdebc5c4698c4db8df33fc40f103b1
dr|z3d <z3d@i2pmail.org>
2022-01-18 13:22:55 +0000

    I2PSnark: fix NPE in logging

--- apps/i2psnark/java/src/org/klomp/snark/TrackerClient.java
+++ apps/i2psnark/java/src/org/klomp/snark/TrackerClient.java
@@ -670,12 +670,9 @@ public class TrackerClient implements Runnable {
                     rv = pids.size();
                     pids.clear();
                 }
-            } else if (meta.isPrivate()) {
-                if (_log.shouldLog(Log.INFO))
-                    _log.info("Not requesting PEX peers for [" + snark.getInfoHash() + "] as torrent is marked private");
             } else {
                 if (_log.shouldLog(Log.INFO))
-                    _log.info("Not requesting PEX peers for [" + snark.getInfoHash() + "] as we have enough already");
+                    _log.info("Not requesting PEX peers for [" + snark.getInfoHash() + "]");
             }
             return rv;
     }
