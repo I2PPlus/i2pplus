cd17e9c489b31cb0e1bc58cd5e9a6a93c9e24d2c
dr|z3d <z3d@i2pmail.org>
2022-03-03 05:03:52 +0000

    Router: add more randomness to build msg timeout and increase base value

--- router/java/src/net/i2p/router/tunnel/pool/BuildRequestor.java
+++ router/java/src/net/i2p/router/tunnel/pool/BuildRequestor.java
@@ -79,7 +79,7 @@ abstract class BuildRequestor {
 
     /** some randomization is added on to this */
 //    private static final int BUILD_MSG_TIMEOUT = 60*1000;
-    private static final int BUILD_MSG_TIMEOUT = 15*1000;
+    private static final int BUILD_MSG_TIMEOUT = 40*1000;
 
 //    private static final int MAX_CONSECUTIVE_CLIENT_BUILD_FAILS = 6;
     private static final int MAX_CONSECUTIVE_CLIENT_BUILD_FAILS = 5;
@@ -294,7 +294,8 @@ abstract class BuildRequestor {
             // send it directly to the first hop
             // Add some fuzz to the TBM expiration to make it harder to guess how many hops
             // or placement in the tunnel
-            msg.setMessageExpiration(ctx.clock().now() + BUILD_MSG_TIMEOUT + ctx.random().nextLong(20*1000));
+//            msg.setMessageExpiration(ctx.clock().now() + BUILD_MSG_TIMEOUT + ctx.random().nextLong(20*1000));
+            msg.setMessageExpiration(ctx.clock().now() + BUILD_MSG_TIMEOUT + ctx.random().nextLong(20*1000) + ctx.random().nextLong(20*1000));
             // We set the OutNetMessage expiration much shorter, so that the
             // TunnelBuildFirstHopFailJob fires before the 13s build expiration.
             RouterInfo peer = ctx.netDb().lookupRouterInfoLocally(cfg.getPeer(1));
