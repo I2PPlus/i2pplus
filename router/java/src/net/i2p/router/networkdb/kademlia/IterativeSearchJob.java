5567aa896cc4febe75c002c455dbb3919e98d4d7
dr|z3d <z3d@i2pmail.org>
2022-03-20 08:54:41 +0000

    Router: tweak iterative search job params

--- router/java/src/net/i2p/router/networkdb/kademlia/IterativeSearchJob.java
+++ router/java/src/net/i2p/router/networkdb/kademlia/IterativeSearchJob.java
@@ -86,19 +86,19 @@ public class IterativeSearchJob extends FloodSearchJob {
     private final Set<Hash> _skippedPeers;
 
 //    private static final int MAX_NON_FF = 3;
-    private static final int MAX_NON_FF = 2;
+    private static final int MAX_NON_FF = 5;
     /** Max number of peers to query */
 //    private static final int TOTAL_SEARCH_LIMIT = 5;
-    private static final int TOTAL_SEARCH_LIMIT = 4;
+    private static final int TOTAL_SEARCH_LIMIT = 12;
     /** Max number of peers to query if we are ff */
-    private static final int TOTAL_SEARCH_LIMIT_WHEN_FF = 3;
+    private static final int TOTAL_SEARCH_LIMIT_WHEN_FF = 6;
     /** Extra peers to get from peer selector, as we may discard some before querying */
 //    private static final int EXTRA_PEERS = 1;
     private static final int EXTRA_PEERS = 2;
     private static final int IP_CLOSE_BYTES = 3;
     /** TOTAL_SEARCH_LIMIT * SINGLE_SEARCH_TIME, plus some extra */
 //    private static final int MAX_SEARCH_TIME = 30*1000;
-    private static final int MAX_SEARCH_TIME = SystemVersion.isSlow() ? 25*1000 : 15*1000;
+    private static final int MAX_SEARCH_TIME = SystemVersion.isSlow() ? 15*1000 : 10*1000;
     /**
      *  The time before we give up and start a new search - much shorter than the message's expire time
      *  Longer than the typ. response time of 1.0 - 1.5 sec, but short enough that we move
@@ -108,7 +108,8 @@ public class IterativeSearchJob extends FloodSearchJob {
     /**
      * The default single search time
      */
-    private static final long SINGLE_SEARCH_TIME = 3*1000;
+//    private static final long SINGLE_SEARCH_TIME = 3*1000;
+    private static final long SINGLE_SEARCH_TIME = 2*1000;
     /** the actual expire time for a search message */
     private static final long SINGLE_SEARCH_MSG_TIME = 10*1000;
     /**
@@ -712,7 +713,7 @@ public class IterativeSearchJob extends FloodSearchJob {
         if (_log.shouldInfo()) {
             long timeRemaining = _expiration - getContext().clock().now();
             _log.info("[Job " + getJobId() + "] IterativeSearch for "  + (_isLease ? "LeaseSet " : "Router") + " [" + _key.toBase64().substring(0,6) + "] failed" +
-                      "\n* Peers queried: " + tries + " Time taken: " + (time / 1000) + "s (" + (timeRemaining / 1000) + "s remaining)");
+                      "\n* Peers queried: " + tries + "; Time taken: " + (time / 1000) + "s (" + (timeRemaining / 1000) + "s remaining)");
         }
         if (tries > 0) {
             // don't bias the stats with immediate fails
@@ -751,7 +752,7 @@ public class IterativeSearchJob extends FloodSearchJob {
         long time = System.currentTimeMillis() - _created;
         if (_log.shouldInfo())
             _log.info("[Job " + getJobId() + "] IterativeSearch for " + (_isLease ? "LeaseSet " : "Router") + " [" + _key.toBase64().substring(0,6) + "] succeeded" +
-                      "\n* Peers queried: " + tries + " Time taken: " + (time / 1000) + "s");
+                      "\n* Peers queried: " + tries + "; Time taken: " + (time / 1000) + "s");
         getContext().statManager().addRateData("netDb.successTime", time);
         getContext().statManager().addRateData("netDb.successRetries", tries - 1);
         for (Job j : _onFind) {
