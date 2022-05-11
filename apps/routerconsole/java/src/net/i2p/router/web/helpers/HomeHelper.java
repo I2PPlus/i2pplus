5a1394b3079bf052dafea0f4faa2d3bcd63fe53c
dr|z3d <z3d@i2pmail.org>
2022-05-11 14:57:31 +0000

    Console: add ddosecrets.i2p to /home

--- apps/routerconsole/java/src/net/i2p/router/web/helpers/HomeHelper.java
+++ apps/routerconsole/java/src/net/i2p/router/web/helpers/HomeHelper.java
@@ -156,6 +156,7 @@ public class HomeHelper extends HelperBase {
         "torch.i2p" + S +_x("Tor-focused Search Engine") + S + "http://torch.i2p/" + S + I + "onionsearch.svg" + S +
 
 // wikis & reference
+        "ddosecrets.i2p" + S + _x("Enabling the free transmission of data in the public interest") + S + "http://ddosecrets.i2p/" + S + I + "wiki.svg" + S +
         "ebooks.i2p" + S + _x("Huge collection of books &amp; magazines &amp; comics") + S + "http://ebooks.i2p/" + S + I + "books.svg" + S +
         "freehaven.i2p" + S + _x("Selected Papers in Anonymity") + S + "http://freehaven.i2p/" + S + I + "books.svg" + S +
         "hiddenwiki.i2p" + S + _x("Tor .onion site links") + S + "http://hiddenwiki.i2p/" + S + I + "wiki.svg" + S +
