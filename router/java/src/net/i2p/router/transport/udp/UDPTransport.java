12cb71a774e4f6c5c507e7ac064a2f245c8f829f
zzz <zzz@i2pmail.org>
2022-06-06 16:20:44 -0400

    SSU: Don't allow android to be introducers
    (cherry picked from commit df4be2687804788bdd586802810a98bddf1d0133)

--- router/java/src/net/i2p/router/transport/udp/UDPTransport.java
+++ router/java/src/net/i2p/router/transport/udp/UDPTransport.java
@@ -3171,6 +3171,7 @@ public class UDPTransport extends TransportImpl implements TimedWeightedPriority
         // count as connections, we have to keep the connection to this peer up longer if
         // we are offering introductions.
         return
+            !SystemVersion.isAndroid() &&
             (!_context.router().isHidden()) &&
             (!introducersRequired(ipv6)) &&
             haveCapacity() &&
