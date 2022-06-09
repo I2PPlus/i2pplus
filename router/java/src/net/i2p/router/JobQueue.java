1651e91c5f7b995d78eabe8cc72650cc5237e62d
dr|z3d <z3d@i2pmail.org>
2022-06-09 23:13:18 +0000

    Router: typo fix

--- router/java/src/net/i2p/router/JobQueue.java
+++ router/java/src/net/i2p/router/JobQueue.java
@@ -617,7 +617,7 @@ public class JobQueue {
                             else if (!SystemVersion.isSlow() && SystemVersion.getCores() >= 8 && timeToWait > 3*1000)
                                 timeToWawit = 3*1000;
                             else if (!SystemVersion.isSlow() && SystemVersion.getCores() >= 4 && timeToWait > 5*1000)
-                                timeToWawit = 5*1000;
+                                timeToWait = 5*1000;
                             else if (timeToWait > 10*1000)
                                 timeToWait = 10*1000;
                             //if (_log.shouldDebug())
