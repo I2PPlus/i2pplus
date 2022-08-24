b2c6ef41587e3f2461e03f54fd0f4c8502983460
dr|z3d <z3d@i2pmail.org>
2022-08-24 16:01:56 +0000

    Router: increase priority blocking queue buffers for non-slow routers

--- router/java/src/net/i2p/router/util/PriBlockingQueue.java
+++ router/java/src/net/i2p/router/util/PriBlockingQueue.java
@@ -32,8 +32,8 @@ public class PriBlockingQueue<E extends PQEntry> extends PriorityBlockingQueue<E
     protected static final long[] RATES = {60*1000, 10*60*1000, 60*60*1000};
 //    protected static final int BACKLOG_SIZE = 256;
 //    protected static final int MAX_SIZE = 512;
-    protected static final int DEFAULT_BACKLOG_SIZE = 512;
-    protected static final int DEFAULT_MAX_SIZE = 1024;
+    protected static final int DEFAULT_BACKLOG_SIZE = SystemVersion.isSlow() ? 512 : 1024;
+    protected static final int DEFAULT_MAX_SIZE = SystemVersion.isSlow() ? 1024 : 4096;
     public static final String PROP_MAX_SIZE = "router.codelMaxQueue";
     public static final String PROP_BACKLOG_SIZE = "router.codelBacklog";
 /*
