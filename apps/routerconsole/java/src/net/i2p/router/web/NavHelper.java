c08e3f633f901ca0c7d99913c2396d57880382eb
zzz <zzz@i2pmail.org>
2022-10-19 15:33:53 -0400

    Console: Make NavHelper non-static
    add NavService interface
    hang off ClientAppManager
    i2psnark: register additional instances with NavService
    
    (cherry picked from commit 0224a0f03b7d374634ea264b9b67caec32cd6a4c)

--- apps/routerconsole/java/src/net/i2p/router/web/NavHelper.java
+++ apps/routerconsole/java/src/net/i2p/router/web/NavHelper.java
@@ -8,16 +8,17 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import net.i2p.I2PAppContext;
-/**
-import net.i2p.router.web.App;
-**/
+import net.i2p.app.ClientApp;
+import net.i2p.app.ClientAppManager;
+import net.i2p.app.ClientAppState;
+import net.i2p.app.NavService;
 
 import net.i2p.router.web.CSSHelper;
 
-public class NavHelper {
+public class NavHelper implements NavService, ClientApp {
     // both indexed by standard (untranslated) app name
-    private static final Map<String, App> _apps = new ConcurrentHashMap<String, App>(4);
-    private static final Map<String, byte[]> _binary = new ConcurrentHashMap<String, byte[]>(4);
+    private final Map<String, App> _apps = new ConcurrentHashMap<String, App>(4);
+    private final Map<String, byte[]> _binary = new ConcurrentHashMap<String, byte[]>(4);
 
     /**
      * To register a new client application so that it shows up on the router
@@ -32,7 +33,7 @@ public class NavHelper {
      * @param iconpath path-only URL starting with /, HTML escaped, or null
      * @since 0.9.20 added iconpath parameter
      */
-    public static void registerApp(String appName, String displayName, String path, String tooltip, String iconpath) {
+    public void registerApp(String appName, String displayName, String path, String tooltip, String iconpath) {
         if (iconpath != null && !iconpath.startsWith("/"))
             iconpath = null;
         _apps.put(appName, new App(displayName, tooltip, path, iconpath));
@@ -41,7 +42,7 @@ public class NavHelper {
     /**
      * @param name standard name for the app
      */
-    public static void unregisterApp(String name) {
+    public void unregisterApp(String name) {
         _apps.remove(name);
     }
 
@@ -51,7 +52,7 @@ public class NavHelper {
      *  @return null if not found
      *  @since 0.9.25
      */
-    public static byte[] getBinary(String name){
+    public byte[] getBinary(String name){
         if (name != null)
             return _binary.get(name);
         else
@@ -63,7 +64,7 @@ public class NavHelper {
      *  @param name plugin name
      *  @since 0.9.25
      */
-    public static void setBinary(String name, byte[] arr){
+    public void setBinary(String name, byte[] arr){
         _binary.put(name, arr);
     }
 
@@ -72,7 +73,7 @@ public class NavHelper {
      *  Translated string is loaded by PluginStarter
      *  @return map of translated name to HTML string, or null if none
      */
-    public static Map<String, String> getClientAppLinks() {
+    public Map<String, String> getClientAppLinks() {
         if (_apps.isEmpty())
             return null;
         Map<String, String> rv = new HashMap<String, String>(_apps.size());
@@ -115,13 +116,9 @@ public class NavHelper {
      *  @param name standard app name
      *  @since 0.9.45
      */
-    private static void getClientAppImg(StringBuilder buf, String name, String iconpath) {
+    private void getClientAppImg(StringBuilder buf, String name, String iconpath) {
             if (iconpath != null) {
                 buf.append("<img src=\"").append(iconpath).append("\"");
-            } else if (name.equals("orchid")) {
-                buf.append("<img src=\"/themes/console/light/images/flower.png\"");
-            } else if (name.equals("i2pbote")) {
-                buf.append("<img src=\"/themes/console/light/images/mail_black.png\"");
             } else {
                 buf.append("<img src=\"/themes/console/images/plugin.svg\"");
             }
@@ -134,7 +131,7 @@ public class NavHelper {
      *  @return non-null, possibly empty, unsorted
      *  @since 0.9, public since 0.9.33, was package private
      */
-    public static List<App> getClientApps(I2PAppContext ctx) {
+    public List<App> getClientApps(I2PAppContext ctx) {
         if (_apps.isEmpty())
             return Collections.emptyList();
         List<App> rv = new ArrayList<App>(_apps.size());
@@ -159,4 +156,45 @@ public class NavHelper {
         }
         return rv;
     }
+
+    /** @since 0.9.56 */
+    public static NavHelper getInstance() {
+        return getInstance(I2PAppContext.getGlobalContext());
+    }
+
+    /** @since 0.9.56 */
+    public static NavHelper getInstance(I2PAppContext ctx) {
+        ClientAppManager cmgr = ctx.clientAppManager();
+        if (cmgr != null)
+            return (NavHelper) cmgr.getRegisteredApp("NavHelper");
+        return null;
+    }
+
+    /////// ClientApp methods
+
+    /** @since 0.9.56 */
+    public void startup() {}
+
+    /** @since 0.9.56 */
+    public void shutdown(String[] args) {
+        _apps.clear();
+        _binary.clear();
+    }
+
+    /** @since 0.9.56 */
+    public ClientAppState getState() {
+        return ClientAppState.RUNNING;
+    }
+
+    /** @since 0.9.56 */
+    public String getName() {
+        return "NavHelper";
+    }
+
+    /** @since 0.9.56 */
+    public String getDisplayName() {
+        return "Nav Helper";
+    }
+
+    /////// end ClientApp methods
 }
