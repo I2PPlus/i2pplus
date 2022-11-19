c4aba395ae495492634169b9b7492d701ba1f35f
dr|z3d <z3d@i2pmail.org>
2022-11-19 02:03:13 +0000

    Router: reduce known routers threshold before skipping expl. search

--- router/java/src/net/i2p/router/networkdb/kademlia/FloodfillNetworkDatabaseFacade.java
+++ router/java/src/net/i2p/router/networkdb/kademlia/FloodfillNetworkDatabaseFacade.java
@@ -594,7 +594,7 @@ public class FloodfillNetworkDatabaseFacade extends KademliaNetworkDatabaseFacad
         long maxMemory = SystemVersion.getMaxMemory();
         // 250 for every 32 MB, min of 250, max of 1250
 //        MAX_DB_BEFORE_SKIPPING_SEARCH = (int) Math.max(250l, Math.min(1250l, maxMemory / ((32 * 1024 * 1024l) / 250)));
-        MAX_DB_BEFORE_SKIPPING_SEARCH = 3500;
+        MAX_DB_BEFORE_SKIPPING_SEARCH = 2500;
     }
 
     /**
