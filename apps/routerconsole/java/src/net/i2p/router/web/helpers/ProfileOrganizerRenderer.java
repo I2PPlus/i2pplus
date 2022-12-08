d2888c93eb24dc805cd797d060caa5b13a109a82
dr|z3d <z3d@i2pmail.org>
2022-12-08 04:44:26 +0000

    Console: revert 59afc44c (Console: Show "--" for values < 1 on profiles tabs)

--- apps/routerconsole/java/src/net/i2p/router/web/helpers/ProfileOrganizerRenderer.java
+++ apps/routerconsole/java/src/net/i2p/router/web/helpers/ProfileOrganizerRenderer.java
@@ -563,15 +563,7 @@ class ProfileOrganizerRenderer {
     }
 
     private final static DecimalFormat _fmt = new DecimalFormat("###,##0.00");
-
-    private final static String num(double num) {
-        if (num >= 1.0d) {
-            synchronized (_fmt) { return _fmt.format(num); }
-        } else {
-            return NA;
-        }
-    }
-
+    private final static String num(double num) { synchronized (_fmt) { return _fmt.format(num); } }
     private final static String NA = "";
 
     private String avg (PeerProfile prof, long rate, RateAverages ra) {
