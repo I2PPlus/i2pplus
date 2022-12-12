669fbacec61d4e3cfed7c0f5ae854356c30b6f9f
dr|z3d <z3d@i2pmail.org>
2022-12-12 18:24:38 +0000

    Router: participating throttler tweaks

--- router/java/src/net/i2p/router/tunnel/pool/ParticipatingThrottler.java
+++ router/java/src/net/i2p/router/tunnel/pool/ParticipatingThrottler.java
@@ -61,7 +61,8 @@ class ParticipatingThrottler {
 //        int limit = Math.max(MIN_LIMIT, Math.min(MAX_LIMIT, numTunnels * PERCENT_LIMIT / 100));
         int limit = isSlow ? Math.max(MAX_LIMIT / 4, numTunnels * (PERCENT_LIMIT / 4) / 100)
                     : !isQuadCore ? Math.max(MAX_LIMIT / 2, numTunnels * (PERCENT_LIMIT / 2) / 100)
-                    : Math.max(MAX_LIMIT, numTunnels * PERCENT_LIMIT / 100);
+                    : SystemVersion.getCores() >= 8 && SystemVersion.getMaxMemory() >= 2 * 1024*1024*1024 ? Math.max(MAX_LIMIT, numTunnels * PERCENT_LIMIT / 100)
+                    : Math.min(MAX_LIMIT, numTunnels * (PERCENT_LIMIT) / 100);
         int count = counter.increment(h);
         Result rv;
         if (count > limit) {
