1dd4af819f48bae4d0b97441f26535968ecc1354
dr|z3d <z3d@i2pmail.org>
2022-12-19 20:28:07 +0000

    I2PTunnel: Replace close/refresh buttons on index with iconified close button;
    Improvements to ajax index refresh script

--- apps/i2ptunnel/jsp/js/refreshIndex.js
+++ apps/i2ptunnel/jsp/js/refreshIndex.js
@@ -1,17 +1,18 @@
 import {initToggleInfo} from "./toggleTunnelInfo.js";
 var control = document.getElementById("globalTunnelControl");
 var toggle = document.getElementById("toggleInfo");
+var messages = document.getElementById("tunnelMessages");
 
 function refreshTunnelStatus(timestamp) {
   var index = document.getElementById("page");
-
   var url = window.location.pathname;
-
   var xhrtunnman = new XMLHttpRequest();
 
-  xhrtunnman.open('GET', url + "?t=" + new Date().getTime(), true);
-  xhrtunnman.setRequestHeader("If-Modified-Since", "Sat, 1 Jan 2000 00:00:00 GMT");
+  //xhrtunnman.open('GET', url + "?t=" + new Date().getTime(), true);
+  xhrtunnman.open('GET', url, true);
+  //xhrtunnman.setRequestHeader("If-Modified-Since", "Sat, 1 Jan 2000 00:00:00 GMT");
   xhrtunnman.responseType = "document";
+  xhrtunnman.setRequestHeader("Cache-Control", "no-cache");
   xhrtunnman.onreadystatechange = function() {
 
     if (xhrtunnman.readyState == 4) {
@@ -22,45 +23,58 @@ function refreshTunnelStatus(timestamp) {
         if (uninit || down) {
           var uninitResponse = xhrtunnman.responseXML.getElementById("notReady");
           var downResponse = xhrtunnman.responseXML.getElementById("notReady");
-          if (uninitResponse || downResponse)
-            window.requestAnimationFrame(refreshAll);
-          else
+          if (uninitResponse || downResponse) {
+            refreshAll();
+          } else {
             reloadPage();
+          }
         } else {
-          window.requestAnimationFrame(updateVolatile);
+          updateVolatile();
         }
 
-        function updateVolatile(timestamp) {
+        function updateVolatile() {
           var updating = document.getElementsByClassName("volatile");
           var updatingResponse = xhrtunnman.responseXML.getElementsByClassName("volatile");
+          if (messages !== null) {
+            var messagesResponse = xhrtunnman.responseXML.getElementById("tunnelMessages");
+            if (!Object.is(messages.innerHTML, messages.innerHTML)) {
+              messages.innerHTML = messagesResponse.innerHTML;
+            }
+          }
           var i;
           for (i = 0; i < updating.length; i++) {
-            if (!Object.is(updating[i].innerHTML, updatingResponse[i].innerHTML))
+            if (!Object.is(updating[i].innerHTML, updatingResponse[i].innerHTML)) {
               updating[i].innerHTML = updatingResponse[i].innerHTML;
-            else if (updating.length != updatingResponse.length)
-              window.requestAnimationFrame(refreshPanels);
+            } else if (updating.length != updatingResponse.length) {
+              refreshPanels();
+            }
           }
         }
 
-        function refreshPanels(timestamp) {
-          var screenlog = document.getElementById("messages");
-          var screenlogResponse = xhrtunnman.responseXML.getElementById("messages");
+        function refreshPanels() {
           var servers = document.getElementById("servers");
           var serversResponse = xhrtunnman.responseXML.getElementById("servers");
           var clients = document.getElementById("clients");
           var clientsResponse = xhrtunnman.responseXML.getElementById("clients");
-          if (!Object.is(screenlog.innerHTML, screenlogResponse.innerHTML))
-            screenlog.innerHTML = screenlogResponse.innerHTML;
-          if (!Object.is(servers.innerHTML, serversResponse.innerHTML))
+          if (messages !== null) {
+            var messagesResponse = xhrtunnman.responseXML.getElementById("tunnelMessages");
+            if (!Object.is(messages.innerHTML, messagesResponse.innerHTML)) {
+              messages.innerHTML = messagesResponse.innerHTML;
+            }
+          }
+          if (!Object.is(servers.innerHTML, serversResponse.innerHTML)) {
             servers.innerHTML = serversResponse.innerHTML;
-          if (!Object.is(clients.innerHTML, clientsResponse.innerHTML))
+          }
+          if (!Object.is(clients.innerHTML, clientsResponse.innerHTML)) {
             clients.innerHTML = clientsResponse.innerHTML;
+          }
         }
 
-        function refreshAll(timestamp) {
+        function refreshAll() {
           var indexResponse = xhrtunnman.responseXML.getElementById("page");
-          if (typeof indexResponse != "undefined" && !Object.is(index.innerHTML, indexResponse.innerHTML))
+          if (typeof indexResponse != "undefined" && !Object.is(index.innerHTML, indexResponse.innerHTML)) {
             index.innerHTML = indexResponse.innerHTML;
+          }
         }
 
         function reloadPage() {
@@ -69,7 +83,7 @@ function refreshTunnelStatus(timestamp) {
 
       } else {
         function noRouter() {
-          var down = "<div id=\"down\"><b><span>Router is down<\/span><\/b><\/div>";
+          var down = "<div id=down><b><span>Router is down<\/span><\/b><\/div>";
           index.innerHTML = down;
         }
         setTimeout(noRouter, 10000)
@@ -80,12 +94,13 @@ function refreshTunnelStatus(timestamp) {
 }
 
 function bindToggle() {
-  if (toggle)
+  if (toggle) {
     toggle.addEventListener("click", initToggleInfo, false);
+  }
 }
 
 function refreshIndex() {
-  window.requestAnimationFrame(refreshTunnelStatus);
+  refreshTunnelStatus();
   bindToggle();
 }
 
