9f4121d2fa2227ac44fc339d51afdc4d7570961d
dr|z3d <z3d@i2pmail.org>
2022-12-21 12:16:29 +0000

    Tunnel Pool: harden participating throttler some more

--- router/java/src/net/i2p/router/tunnel/pool/ParticipatingThrottler.java
+++ router/java/src/net/i2p/router/tunnel/pool/ParticipatingThrottler.java
@@ -38,10 +38,10 @@ class ParticipatingThrottler {
     private static final int LIFETIME_PORTION = 3;
 //    private static final int MIN_LIMIT = 18 / LIFETIME_PORTION;
 //    private static final int MAX_LIMIT = 66 / LIFETIME_PORTION;
-    private static final int MIN_LIMIT = 64 / LIFETIME_PORTION;
-    private static final int MAX_LIMIT = 256 / LIFETIME_PORTION;
+    private static final int MIN_LIMIT = 36 / LIFETIME_PORTION;
+    private static final int MAX_LIMIT = 192 / LIFETIME_PORTION;
 //    private static final int PERCENT_LIMIT = 3 / LIFETIME_PORTION;
-    private static final int PERCENT_LIMIT = 10 / LIFETIME_PORTION;
+    private static final int PERCENT_LIMIT = 12 / LIFETIME_PORTION;
     private static final long CLEAN_TIME = 11*60*1000 / LIFETIME_PORTION;
     private boolean isSlow = SystemVersion.isSlow();
     private boolean isQuadCore = SystemVersion.getCores() >=4;
@@ -59,10 +59,11 @@ class ParticipatingThrottler {
     Result shouldThrottle(Hash h) {
         int numTunnels = this.context.tunnelManager().getParticipatingCount();
 //        int limit = Math.max(MIN_LIMIT, Math.min(MAX_LIMIT, numTunnels * PERCENT_LIMIT / 100));
-        int limit = isSlow ? Math.max(MAX_LIMIT / 5, numTunnels * (PERCENT_LIMIT / 5) / 100)
-                    : !isQuadCore ? Math.max(MAX_LIMIT / 4, numTunnels * (PERCENT_LIMIT / 4) / 100)
-                    : SystemVersion.getCores() >= 8 && SystemVersion.getMaxMemory() >= 2 * 1024*1024*1024 ? Math.max(MAX_LIMIT / 2, numTunnels * (PERCENT_LIMIT / 2) / 100)
-                    : Math.max(MAX_LIMIT / 3, numTunnels * (PERCENT_LIMIT / 3) / 100);
+        int limit = isSlow ? Math.min(MIN_LIMIT, Math.max(MAX_LIMIT / 5, numTunnels * (PERCENT_LIMIT / 5) / 100))
+                    : !isQuadCore ? Math.min((MIN_LIMIT * 3 / 2), Math.max(MAX_LIMIT / 4, numTunnels * (PERCENT_LIMIT / 4) / 100))
+                    : SystemVersion.getCores() >= 8 && SystemVersion.getMaxMemory() >= 2 * 1024*1024*1024 ?
+                      Math.min((MIN_LIMIT * 3), Math.max(MAX_LIMIT / 2, numTunnels * (PERCENT_LIMIT / 2) / 100))
+                    : Math.min(MIN_LIMIT * 2,(Math.max(MAX_LIMIT / 3, numTunnels * (PERCENT_LIMIT / 3) / 100)));
         int count = counter.increment(h);
         Result rv;
         if (count > limit) {
