f3fcfc0486ab463d6ff25a35ac6aecb9cde808d8
dr|z3d <z3d@i2pmail.org>
2023-01-05 14:29:48 +0000

    Router: fix NPEs in build handler

--- router/java/src/net/i2p/router/tunnel/pool/BuildHandler.java
+++ router/java/src/net/i2p/router/tunnel/pool/BuildHandler.java
@@ -775,10 +775,12 @@ class BuildHandler implements Runnable {
                 _context.statManager().addRateData("tunnel.rejectHostile", 1);
                 if (_log.shouldWarn())
                     _log.warn("Dropping hostile build request, we are the previous hop: " + req);
-                _context.commSystem().mayDisconnect(from);
-                _context.banlist().banlistRouter(from, " <b>➜</b> HostileTunnel Request (previous hop)", null, null, _context.clock().now() + bantime);
-                _log.warn("Temp banning [" + from.toBase64().substring(0,6) + "] for " + period +
-                          "m -> Hostile tunnel request (We are the previous hop!)");
+                if (from != null) {
+                    _context.commSystem().mayDisconnect(from);
+                    _context.banlist().banlistRouter(from, " <b>➜</b> HostileTunnel Request (previous hop)", null, null, _context.clock().now() + bantime);
+                    _log.warn("Temp banning [" + from.toBase64().substring(0,6) + "] for " + period +
+                              "m -> Hostile tunnel request (We are the previous hop!)");
+                }
                 return;
             }
         }
@@ -790,10 +792,12 @@ class BuildHandler implements Runnable {
                 _context.statManager().addRateData("tunnel.rejectHostile", 1);
                 if (_log.shouldWarn())
                     _log.warn("Dropping hostile build request with the same previous and next hop: " + req);
-                _context.commSystem().mayDisconnect(from);
-                _context.banlist().banlistRouter(from, " <b>➜</b> HostileTunnel Request (duplicate hops in chain)", null, null, _context.clock().now() + bantime);
-                _log.warn("Temp banning [" + from.toBase64().substring(0,6) + "] for " + period +
-                          "m -> Hostile tunnel request (duplicate hops in chain)");
+                if (from != null) {
+                    _context.commSystem().mayDisconnect(from);
+                    _context.banlist().banlistRouter(from, " <b>➜</b> HostileTunnel Request (duplicate hops in chain)", null, null, _context.clock().now() + bantime);
+                    _log.warn("Temp banning [" + from.toBase64().substring(0,6) + "] for " + period +
+                              "m -> Hostile tunnel request (duplicate hops in chain)");
+                }
                 return;
             }
         }
