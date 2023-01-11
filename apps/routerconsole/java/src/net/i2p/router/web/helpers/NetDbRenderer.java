434142e693c0bd092e11b141f2324463cda664d7
zzz <zzz@i2pmail.org>
2023-01-11 13:44:42 -0500

    Console: Add ability to search netdb for a range of ports
    Minor cleanup of dup strings
    
    (cherry picked from commit f814b85a86bf1e2a4252f1b7be484181cc06f6e3)

--- apps/routerconsole/java/src/net/i2p/router/web/helpers/NetDbRenderer.java
+++ apps/routerconsole/java/src/net/i2p/router/web/helpers/NetDbRenderer.java
@@ -106,11 +106,12 @@ class NetDbRenderer {
      *  @param version may be null
      *  @param country may be null
      *  @param family may be null
+     *  @param highPort if nonzero, a range from port to highPort inclusive
      */
     public void renderRouterInfoHTML(Writer out, int pageSize, int page,
                                      String routerPrefix, String version,
                                      String country, String family, String caps,
-                                     String ip, String sybil, int port, SigType type, EncType etype,
+                                     String ip, String sybil, int port, int highPort, SigType type, EncType etype,
                                      String mtu, String ipv6, String ssucaps,
                                      String tr, int cost) throws IOException {
         StringBuilder buf = new StringBuilder(4*1024);
@@ -357,7 +358,9 @@ class NetDbRenderer {
                         }
                     } else if (port != 0) {
                         for (RouterAddress ra : ri.getAddresses()) {
-                            if (port == ra.getPort()) {
+                        int raport = ra.getPort();
+                        if (port == raport ||
+                            (highPort > 0 && raport >= port && raport <= highPort)) {
                                 if (skipped < toSkip) {
                                     skipped++;
                                     break;
@@ -469,8 +472,12 @@ class NetDbRenderer {
                     buf.append("IP ").append(ip).append(' ');
                 if (ipv6 != null)
                     buf.append("IP ").append(ipv6).append(' ');
-                if (port != 0)
-                    buf.append(_t("Port")).append(' ').append(port).append(' ');
+                if (port != 0) {
+                    buf.append(_t("Port")).append(' ').append(port);
+                    if (highPort != 0)
+                        buf.append('-').append(highPort);
+                    buf.append(' ');
+                }
                 if (mtu != null)
                     buf.append(_t("MTU")).append(' ').append(mtu).append(' ');
                 if (cost != 0)
