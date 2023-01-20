369f8bd75fd89127890f27416c85f6f1ddfabeaa
dr|z3d <z3d@i2pmail.org>
2023-01-20 22:31:14 +0000

    Router: reduce interval between expire leases jobs

--- router/java/src/net/i2p/router/networkdb/kademlia/ExpireLeasesJob.java
+++ router/java/src/net/i2p/router/networkdb/kademlia/ExpireLeasesJob.java
@@ -31,7 +31,7 @@ class ExpireLeasesJob extends JobImpl {
     private final KademliaNetworkDatabaseFacade _facade;
 
 //    private final static long RERUN_DELAY_MS = 1*60*1000;
-    private final static long RERUN_DELAY_MS = 50*1000;
+    private final static long RERUN_DELAY_MS = 30*1000;
 
     public ExpireLeasesJob(RouterContext ctx, KademliaNetworkDatabaseFacade facade) {
         super(ctx);
