3b10ac12549d42af44b40f24b01cf4d48f1b5b00
dr|z3d <z3d@i2pmail.org>
2023-01-28 21:17:59 +0000

    Router: disable novg.net reseed host while offline

--- router/java/src/net/i2p/router/networkdb/reseed/Reseeder.java
+++ router/java/src/net/i2p/router/networkdb/reseed/Reseeder.java
@@ -121,7 +121,7 @@ public class Reseeder {
         "https://reseed-pl.i2pd.xyz/"               + ',' +   // r4sas-reseed_at_mail.i2p.crt      CA
         "https://reseed.diva.exchange/"             + ',' +   // reseed_at_diva.exchange.crt       CA
         "https://reseed.i2pgit.org/"                + ',' +   // hankhill19580_at_gmail.com.crt    CA                                Java 8+
-        "https://i2p.novg.net/"                     + ',' +   // igor_at_novg.net.crt              CA                                Java 8+
+        //"https://i2p.novg.net/"                     + ',' +   // igor_at_novg.net.crt              CA                                Java 8+
         "https://i2pseed.creativecowpat.net:8443/"  + ',' +   // creativecowpat_at_mail.i2p.crt    i2pseed.creativecowpat.net.crt    Java 7+
         "https://reseed.onion.im/"                  + ',' +   // lazygravy_at_mail.i2p             CA                                Java 8+
         "https://reseed.memcpy.io/"                 + ',' +   // hottuna_at_mail.i2p.crt           CA                                SNI required
