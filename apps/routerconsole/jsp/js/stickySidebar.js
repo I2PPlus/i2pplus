15a01cca1b656490ed7537d9b6b0a10b946966dd
dr|z3d <z3d@i2pmail.org>
2023-01-28 06:15:46 +0000

    Console: conditional sticky sidebar

---
+++ apps/routerconsole/jsp/js/stickySidebar.js
@@ -0,0 +1,20 @@
+function stickySidebar() {
+  var html = document.querySelector("html");
+  var htmlHeight = html.getBoundingClientRect().height;
+  var sb = document.getElementById("sidebar");
+  var sbHeight = sb.getBoundingClientRect().height;
+  var sbWrap = document.getElementById("sb_wrap");
+  var viewportHeight = window.visualViewport.height;
+
+  if ((sbHeight + 5) < viewportHeight && htmlHeight > viewportHeight) {
+    sbWrap.style.position = "sticky";
+    sbWrap.style.top = "5px";
+    sbWrap.classList.add("sticky");
+  } else {
+    sbWrap.style.position = null;
+    sbWrap.style.top = null;
+    sbWrap.classList.remove("sticky");
+  }
+}
+
+export {stickySidebar};
