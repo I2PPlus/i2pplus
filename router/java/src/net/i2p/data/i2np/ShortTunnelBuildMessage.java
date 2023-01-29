3dcbd65d32297d18d0e95e4eb743a8e531da4485
dr|z3d <z3d@i2pmail.org>
2023-01-29 08:39:15 +0000

    Router: logging tweak

--- router/java/src/net/i2p/data/i2np/ShortTunnelBuildMessage.java
+++ router/java/src/net/i2p/data/i2np/ShortTunnelBuildMessage.java
@@ -75,7 +75,7 @@ public class ShortTunnelBuildMessage extends TunnelBuildMessage {
     @Override
     public String toString() {
         StringBuilder buf = new StringBuilder(64);
-        buf.append("ShortTunnelBuildMessageID: ").append(getUniqueId()).append("; Records: ").append(getRecordCount());
+        buf.append("\n* ShortTunnelBuildMessageID: ").append(getUniqueId()).append("; Records: ").append(getRecordCount());
         return buf.toString();
     }
 }
