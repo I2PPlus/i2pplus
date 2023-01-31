19b6977e66b37dbcbcaceca93d2d4d473103362c
dr|z3d <z3d@i2pmail.org>
2023-01-31 02:36:21 +0000

    Console: handle iframed class injection better

--- apps/routerconsole/jsp/js/iframedClassInject.js
+++ apps/routerconsole/jsp/js/iframedClassInject.js
@@ -1,14 +1,14 @@
 // inject iframed class to embedded body tag
 
 function injectClass(f) {
-  f.className += ' iframed ';
   var doc = 'contentDocument' in f? f.contentDocument : f.contentWindow.document;
-  if (!doc.body.classList.contains("iframed"))
-    doc.body.className += ' iframed';
+  if (!doc.body.classList.contains("iframed")) {
+    doc.body.classList.add("iframed");
+  }
 }
 
 function iframeResizer(f) {
-  iFrameResize({log: false, interval: 0, heightCalculationMethod: 'taggedElement', warningTimeout: 0}, f);
+  iFrameResize({interval: 0, heightCalculationMethod: 'taggedElement', warningTimeout: 0}, f);
 }
 
 document.addEventListener("DOMContentLoaded", function() {
