86011cdf008cc9217454d3b1e4cdc2fca0b48bf6
dr|z3d <z3d@i2pmail.org>
2023-02-12 21:09:59 +0000

    Console: add . to hidden span to aid job queue sorting

--- apps/routerconsole/java/src/net/i2p/router/web/helpers/JobQueueHelper.java
+++ apps/routerconsole/java/src/net/i2p/router/web/helpers/JobQueueHelper.java
@@ -253,15 +253,15 @@ public class JobQueueHelper extends HelperBase {
             buf.append("<td><b>").append(stats.getName()).append("</b></td>");
             buf.append("<td>").append(stats.getRuns()).append("</td>");
             buf.append("<td>").append(stats.getDropped()).append("</td>");
-            buf.append("<td><span hidden>[").append(stats.getTotalTime()).append("]</span>").append(DataHelper.formatDuration2(stats.getTotalTime())).append("</td>");
-            buf.append("<td><span hidden>[").append(stats.getAvgTime()).append("]</span>").append(DataHelper.formatDuration2(stats.getAvgTime())).append("</td>");
-            buf.append("<td><span hidden>[").append(stats.getMaxTime()).append("]</span>").append(DataHelper.formatDuration2(stats.getMaxTime())).append("</td>");
-            buf.append("<td><span hidden>[").append(stats.getMinTime()).append("]</span>").append(DataHelper.formatDuration2(stats.getMinTime())).append("</td>");
+            buf.append("<td><span hidden>[").append(stats.getTotalTime()).append(".]</span>").append(DataHelper.formatDuration2(stats.getTotalTime())).append("</td>");
+            buf.append("<td><span hidden>[").append(stats.getAvgTime()).append(".]</span>").append(DataHelper.formatDuration2(stats.getAvgTime())).append("</td>");
+            buf.append("<td><span hidden>[").append(stats.getMaxTime()).append(".]</span>").append(DataHelper.formatDuration2(stats.getMaxTime())).append("</td>");
+            buf.append("<td><span hidden>[").append(stats.getMinTime()).append(".]</span>").append(DataHelper.formatDuration2(stats.getMinTime())).append("</td>");
             if (isAdvanced()) {
-                buf.append("<td><span hidden>[").append(stats.getTotalPendingTime()).append("]</span>").append(DataHelper.formatDuration2(stats.getTotalPendingTime())).append("</td>");
-                buf.append("<td><span hidden>[").append(stats.getAvgPendingTime()).append("]</span>").append(DataHelper.formatDuration2(stats.getAvgPendingTime())).append("</td>");
-                buf.append("<td><span hidden>[").append(stats.getMaxPendingTime()).append("]</span>").append(DataHelper.formatDuration2(stats.getMaxPendingTime())).append("</td>");
-                buf.append("<td><span hidden>[").append(stats.getMinPendingTime()).append("]</span>").append(DataHelper.formatDuration2(stats.getMinPendingTime())).append("</td>");
+                buf.append("<td><span hidden>[").append(stats.getTotalPendingTime()).append(".]</span>").append(DataHelper.formatDuration2(stats.getTotalPendingTime())).append("</td>");
+                buf.append("<td><span hidden>[").append(stats.getAvgPendingTime()).append(".]</span>").append(DataHelper.formatDuration2(stats.getAvgPendingTime())).append("</td>");
+                buf.append("<td><span hidden>[").append(stats.getMaxPendingTime()).append(".]</span>").append(DataHelper.formatDuration2(stats.getMaxPendingTime())).append("</td>");
+                buf.append("<td><span hidden>[").append(stats.getMinPendingTime()).append(".]</span>").append(DataHelper.formatDuration2(stats.getMinPendingTime())).append("</td>");
             }
             buf.append("</tr>\n");
             if (stats.getMaxTime() > maxExecTime)
