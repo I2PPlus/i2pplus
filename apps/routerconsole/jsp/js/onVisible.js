32c77c4e5b6daf7456cd5a1d7d6c417d93767e2b
dr|z3d <z3d@i2pmail.org>
2023-03-01 05:04:58 +0000

    Console: cleanup onVisible.js

--- apps/routerconsole/jsp/js/onVisible.js
+++ apps/routerconsole/jsp/js/onVisible.js
@@ -1,12 +1,9 @@
 function onVisible(element, callback) {
   new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
-      if (entry.intersectionRatio > 0) {
+      if (entry.intersectionRatio > 0 && !document.hidden) {
         callback(element);
         observer.disconnect();
-      } else {
-        if (typeof timerId !== "null" && typeof timerId !== "undefined") {clearInterval(timerId);}
-        if (typeof refreshId !== "null" && typeof refreshId !== "undefined") {clearInterval(refreshId);}
       }
     });
   }).observe(element);
