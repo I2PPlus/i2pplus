c994d760bef55f2cb2a681e8136d3af22db9fb4b
dr|z3d <z3d@i2pmail.org>
2023-03-19 03:13:25 +0000

    Router: logging tweak

--- router/java/src/net/i2p/router/tunnel/pool/ParticipatingThrottler.java
+++ router/java/src/net/i2p/router/tunnel/pool/ParticipatingThrottler.java
@@ -114,13 +114,13 @@ class ParticipatingThrottler {
             context.simpleTimer2().addEvent(new Disconnector(h), 3*1000);
             rv = Result.DROP;
             if (_log.shouldWarn())
-                _log.warn("Ignoring tunnel request from Router [" + h.toBase64().substring(0,6) +
+                _log.warn("Ignoring tunnel request from [" + h.toBase64().substring(0,6) +
                           "] -> " + v + (caps != "" ? " / " + caps : ""));
         } else if (VersionComparator.comp(v, MIN_VERSION) < 0 && isUnreachable) {
             context.simpleTimer2().addEvent(new Disconnector(h), 3*1000);
             rv = Result.DROP;
             if (_log.shouldWarn())
-                _log.warn("Ignoring tunnel request from Router [" + h.toBase64().substring(0,6) +
+                _log.warn("Ignoring tunnel request from [" + h.toBase64().substring(0,6) +
                           "] -> " + v + (caps != "" ? " / " + caps : ""));
         }
 
