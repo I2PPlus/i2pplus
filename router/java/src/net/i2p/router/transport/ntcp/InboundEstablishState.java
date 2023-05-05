e196832a44bc4e13c365cdfcd25f2f62e978cc8d
dr|z3d <z3d@i2pmail.org>
2023-05-05 08:26:37 +0000

    Router: only informally tag routers with long-lived bans as hostile

--- router/java/src/net/i2p/router/transport/ntcp/InboundEstablishState.java
+++ router/java/src/net/i2p/router/transport/ntcp/InboundEstablishState.java
@@ -205,14 +205,14 @@ class InboundEstablishState extends EstablishBase implements NTCP2Payload.Payloa
             return false;
         } else if (_context.banlist().isBanlistedHostile(aliceHash) || _context.banlist().isBanlisted(aliceHash)) {
             _context.commSystem().mayDisconnect(aliceHash);
-            boolean isHostile = _context.banlist().isBanlisted(aliceHash);
+            boolean isHostile = _context.banlist().isBanlistedHostile(aliceHash);
             if (_log.shouldWarn())
                 _log.warn("Router [" + aliceHash.toBase64().substring(0,6) + "] is temp banned" +
                           (isHostile ? " and tagged as hostile" : "") + ", validating anyway...");
             return true;
         }
 
-        if(ip != null)
+        if (ip != null)
            _transport.setIP(aliceHash, ip);
         if (_log.shouldDebug())
             _log.debug(prefix() + "verification successful for " + _con);
