dff43bdfdc1b5775bd704bfab8b06756ee04f584
dr|z3d <z3d@i2pmail.org>
2023-06-26 06:50:39 +0000

    I2PTunnel: fix status counters regression

--- apps/i2ptunnel/jsp/js/refreshIndex.js
+++ apps/i2ptunnel/jsp/js/refreshIndex.js
@@ -1,103 +1,48 @@
 import {initToggleInfo} from "./toggleTunnelInfo.js";
 var control = document.getElementById("globalTunnelControl");
-var countServer = document.getElementById("countServer");
 var countClient = document.getElementById("countClient");
+var countServer = document.getElementById("countServer");
+var index = document.getElementById("page");
 var messages = document.getElementById("tunnelMessages");
+var notReady = document.getElementById("notReady");
 var toggle = document.getElementById("toggleInfo");
+var url = window.location.pathname;
+var xhrtunnman = new XMLHttpRequest();
 
-function refreshTunnelStatus(timestamp) {
-  var index = document.getElementById("page");
-  var url = window.location.pathname;
-  var xhrtunnman = new XMLHttpRequest();
-
+function refreshTunnelStatus() {
+  var isDown = document.getElementById("down");
   xhrtunnman.open('GET', url, true);
   xhrtunnman.responseType = "document";
   xhrtunnman.setRequestHeader("Cache-Control", "no-cache");
   xhrtunnman.onreadystatechange = function() {
-
     if (xhrtunnman.readyState == 4) {
       if (xhrtunnman.status == 200) {
-
-        var uninit = document.getElementById("notReady");
-        var down = document.getElementById("down");
-        if (uninit || down) {
-          var uninitResponse = xhrtunnman.responseXML.getElementById("notReady");
-          var downResponse = xhrtunnman.responseXML.getElementById("down");
-          if (uninitResponse || downResponse) {
-            refreshAll();
-          } else {
-            reloadPage();
-          }
+        if (isDown && xhrtunnman.responseXML.getElementById("globalTunnelControl") !== null) {
+          reloadPage();
+        } else if (notReady) {
+          var notReadyResponse = xhrtunnman.responseXML.getElementById("notReady");
+          if (notReadyResponse) {refreshAll();}
+          else {reloadPage();}
         } else {
           updateVolatile();
         }
-
-        function updateVolatile() {
-          var updating = document.getElementsByClassName("volatile");
-          var updatingResponse = xhrtunnman.responseXML.getElementsByClassName("volatile");
-          if (messages !== null) {
-            var messagesResponse = xhrtunnman.responseXML.getElementById("tunnelMessages");
-            if (!Object.is(messages.innerHTML, messages.innerHTML)) {
-              messages.innerHTML = messagesResponse.innerHTML;
-            }
-          }
-          var i;
-          for (i = 0; i < updating.length; i++) {
-            if (!Object.is(updating[i].innerHTML, updatingResponse[i].innerHTML)) {
-              updating[i].innerHTML = updatingResponse[i].innerHTML;
-            } else if (updating.length != updatingResponse.length) {
-              refreshPanels();
-            }
-          }
-          countServices();
-        }
-
-        function refreshPanels() {
-          var servers = document.getElementById("servers");
-          var serversResponse = xhrtunnman.responseXML.getElementById("servers");
-          var clients = document.getElementById("clients");
-          var clientsResponse = xhrtunnman.responseXML.getElementById("clients");
-          if (messages !== null) {
-            var messagesResponse = xhrtunnman.responseXML.getElementById("tunnelMessages");
-            if (!Object.is(messages.innerHTML, messagesResponse.innerHTML)) {
-              messages.innerHTML = messagesResponse.innerHTML;
-            }
-          }
-          if (!Object.is(servers.innerHTML, serversResponse.innerHTML)) {
-            servers.innerHTML = serversResponse.innerHTML;
-          }
-          if (!Object.is(clients.innerHTML, clientsResponse.innerHTML)) {
-            clients.innerHTML = clientsResponse.innerHTML;
-          }
-        }
-
-        function refreshAll() {
-          var indexResponse = xhrtunnman.responseXML.getElementById("page");
-          if (typeof indexResponse != "undefined" && !Object.is(index.innerHTML, indexResponse.innerHTML)) {
-            index.innerHTML = indexResponse.innerHTML;
-          }
-        }
-
-        function reloadPage() {
-          location.reload(true);
-        }
-
       } else {
+        setTimeout(noRouter, 10000);
         function noRouter() {
-          var down = "<div id=down class=notReady><b><span>Router is down<\/span><\/b><\/div>";
+          var down = "<div id=down class=notReady><b><span>Router is down</span></b></div>";
           index.innerHTML = down;
+          if (isDown && xhrtunnman.responseXML.getElementById("globalTunnelControl") !== null) {
+            reloadPage();
+          }
         }
-        setTimeout(noRouter, 10000)
       }
     }
   }
-  var notReady = document.getElementsByClassName("notReady");
-  if (!notReady) {countServices();}
+  countServices();
   xhrtunnman.send();
 }
 
 function countServices() {
-  var notReady = document.getElementsByClassName("notReady");
   var runningClientCount = document.querySelector("#countClient .running");
   var runningClient = document.getElementsByClassName("cli statusRunning");
   var runningServerCount = document.querySelector("#countServer .running");
@@ -114,57 +59,92 @@ function countServices() {
   var stoppedClient = document.getElementsByClassName("cli statusNotRunning");
   var stoppedServerCount = document.querySelector("#countServer .stopped");
   var stoppedServer = document.getElementsByClassName("svr statusNotRunning");
-  if (!notReady) {
+  if (control !== null) {
     if (runningClient !== null && runningClient.length > 0) {
       runningClientCount.innerHTML = " x " + runningClient.length;
-      runningClientCount.hidden = false;
-    } else if (runningClient !== null) {
+    } else if (runningClient !== null && runningClientCount !== null) {
       runningClientCount.innerHTML == "";
-      runningClientCount.hidden = true;
     }
     if (runningServer !== null && runningServer.length > 0) {
       runningServerCount.innerHTML = " x " + runningServer.length;
-      runningServerCount.hidden = false;
-    } else if (runningServer !== null) {
+    } else if (runningServer !== null && runningServerCount !== null) {
       runningServerCount.innerHTML = "";
-      runningServerCount.hidden = true;
     }
     if (standbyClient !== null && standbyClient.length > 0) {
       standbyClientCount.innerHTML = " x " + standbyClient.length;
-      standbyClientCount.hidden = false;
-    } else if (standbyClient !== null) {
+    } else if (standbyClient !== null && standbyClientCount !== null) {
       standbyClientCount.innerHTML = "";
-      standbyClientCount.hidden = true;
     }
     if (startingClient !== null && startingClient.length > 0) {
       startingClientCount.innerHTML = " x " + startingClient.length;
-      startingClientCount.hidden = false;
-    } else if (startingClient !== null) {
+    } else if (startingClient !== null && startingClientCount !== null) {
       startingClientCount.innerHTML = "";
-      startingClientCount.hidden = true;
     }
     if (startingServer !== null && startingServer.length > 0) {
       startingServerCount.innerHTML = " x " + startingServer.length;
-      startingServerCount.hidden = false;
-    } else if (startingServer !== null) {
+    } else if (startingServer !== null && startingServerCount !== null) {
       startingServerCount.innerHTML = "";
-      startingServerCount.hidden = true;
     }
     if (stoppedClient !== null && stoppedClient.length > 0) {
       stoppedClientCount.innerHTML = " x " + stoppedClient.length;
-      stoppedClientCount.hidden = false;
-    } else if (stoppedClient !== null) {
+    } else if (stoppedClient !== null && stoppedClientCount !== null) {
       stoppedClientCount.innerHTML = "";
-      stoppedClientCount.hidden = true;
     }
     if (stoppedServer !== null && stoppedServer.length > 0) {
       stoppedServerCount.innerHTML = " x " + stoppedServer.length;
-      stoppedServerCount.hidden = false;
-    } else if (stoppedServer !== null) {
+    } else if (stoppedServer !== null && stoppedServerCount !== null) {
       stoppedServerCount.innerHTML = "";
-      stoppedServerCount.hidden = true;
     }
   }
+}
+
+function updateVolatile() {
+  var updating = document.getElementsByClassName("volatile");
+  var updatingResponse = xhrtunnman.responseXML.getElementsByClassName("volatile");
+  if (messages !== null) {
+    var messagesResponse = xhrtunnman.responseXML.getElementById("tunnelMessages");
+    if (!Object.is(messages.innerHTML, messages.innerHTML)) {
+      messages.innerHTML = messagesResponse.innerHTML;
+    }
+  }
+  var i;
+  for (i = 0; i < updating.length; i++) {
+    if (!Object.is(updating[i].innerHTML, updatingResponse[i].innerHTML)) {
+      updating[i].innerHTML = updatingResponse[i].innerHTML;
+    } else if (updating.length != updatingResponse.length) {
+      refreshPanels();
+    }
+  }
+}
+
+function refreshPanels() {
+  var servers = document.getElementById("serverTunnels");
+  var serversResponse = xhrtunnman.responseXML.getElementById("serverTunnels");
+  var clients = document.getElementById("clientTunnels");
+  var clientsResponse = xhrtunnman.responseXML.getElementById("clientTunnels");
+  if (messages !== null) {
+    var messagesResponse = xhrtunnman.responseXML.getElementById("tunnelMessages");
+    if (!Object.is(messages.innerHTML, messagesResponse.innerHTML)) {
+      messages.innerHTML = messagesResponse.innerHTML;
+    }
+  }
+  if (!Object.is(servers.innerHTML, serversResponse.innerHTML)) {
+    servers.innerHTML = serversResponse.innerHTML;
+  }
+  if (!Object.is(clients.innerHTML, clientsResponse.innerHTML)) {
+    clients.innerHTML = clientsResponse.innerHTML;
+  }
+}
+
+function refreshAll() {
+  var indexResponse = xhrtunnman.responseXML.getElementById("page");
+  if (typeof indexResponse != "undefined" && !Object.is(index.innerHTML, indexResponse.innerHTML)) {
+    index.innerHTML = indexResponse.innerHTML;
+  }
+}
+
+function reloadPage() {
+  location.reload(true);
 }
 
 function bindToggle() {
