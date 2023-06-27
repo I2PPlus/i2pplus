b58e2e75d00e3e33ad95f181d56eca01f5206e52
dr|z3d <z3d@i2pmail.org>
2023-06-27 19:30:40 +0000

    bump to -51+

--- router/java/src/net/i2p/router/RouterVersion.java
+++ router/java/src/net/i2p/router/RouterVersion.java
@@ -18,7 +18,7 @@ public class RouterVersion {
     /** deprecated */
     public final static String ID = "Git";
     public final static String VERSION = CoreVersion.VERSION;
-    public final static long BUILD = 50;
+    public final static long BUILD = 51;
 
     /** for example "-test" */
     public final static String EXTRA = "+";
