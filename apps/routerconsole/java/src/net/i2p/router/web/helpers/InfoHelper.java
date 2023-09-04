3d0967e73bf5f94fd9a16d13fd1d12edfed3788e
dr|z3d <z3d@i2pmail.org>
2023-09-04 21:49:50 +0000

    Console: update max entries field on /info for rdns to reflect larger cache if maxram >=512M

--- apps/routerconsole/java/src/net/i2p/router/web/helpers/InfoHelper.java
+++ apps/routerconsole/java/src/net/i2p/router/web/helpers/InfoHelper.java
@@ -227,9 +227,10 @@ public class InfoHelper extends HelperBase {
         Boolean rdnsEnabled = _context.getBooleanProperty("routerconsole.enableReverseLookups");
         if (rdnsEnabled) {
             int rdnsCacheSize = CommSystemFacadeImpl.countRdnsCacheEntries();
-            String rdnsFileSize = _context.router().getUptime() > 5 * 1000 ? CommSystemFacadeImpl.rdnsCacheSize() : _t("initializing") + "&hellip;";
-            buf.append("<tr><td><b>RDNS Cache:</b></td><td class=ajax>" + rdnsCacheSize + " / 16384 entries &ensp;" +
-                       "<b>" + _t("Cache file") + ":</b> " + configDir + "rdnscache.txt (" + rdnsFileSize + ")</td></tr>\n");
+            long maxMem = SystemVersion.getMaxMemory();
+            String rdnsFileSize = (_context.router().getUptime() > 5 * 1000 ? CommSystemFacadeImpl.rdnsCacheSize() : _t("initializing") + "&hellip;");
+            buf.append("<tr><td><b>RDNS Cache:</b></td><td class=ajax>" + rdnsCacheSize + " / " + (maxMem < 512*1024*1024 ? "16384" : "32768") + " " +
+                       _t("entries") + "&ensp;<b>" + _t("Cache file") + ":</b> " + configDir + "rdnscache.txt (" + rdnsFileSize + ")</td></tr>\n");
         }
         if (firstInstalled() != null && firstVersion() != null && lastUpdated() != null) {
             buf.append("<tr><td><b>" + _t("Installed") + ":</b></td><td>" + installDate + " (" + firstVersion() + ")" +
