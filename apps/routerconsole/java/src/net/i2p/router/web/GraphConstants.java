bf7a29b73bff3d2c4258632fa3b14d670c75e239
dr|z3d <z3d@i2pmail.org>
2023-09-20 01:29:47 +0000

    Console: source cleanup

--- apps/routerconsole/java/src/net/i2p/router/web/GraphConstants.java
+++ apps/routerconsole/java/src/net/i2p/router/web/GraphConstants.java
@@ -11,9 +11,6 @@ public class GraphConstants {
 
     public static final int DEFAULT_X = 400;
     public static final int DEFAULT_Y = 100;
-    // 4K
-    //public static final int MAX_X = 3840;
-    //public static final int MAX_Y = 2160;
     // 8K
     public static final int MAX_X = 7680;
     public static final int MAX_Y = 4320;
