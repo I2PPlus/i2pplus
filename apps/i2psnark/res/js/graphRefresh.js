2bea24e26756b2047646e568891abb389bfb7618
dr|z3d <z3d@i2pmail.org>
2023-09-21 12:12:13 +0000

    I2PSnark: set mimimum interval for graph refresh to 5m

--- apps/i2psnark/res/js/graphRefresh.js
+++ apps/i2psnark/res/js/graphRefresh.js
@@ -5,30 +5,46 @@
 var graphcss = document.getElementById("graphcss");
 var noload = document.getElementById("noload");
 var xhrGraph;
-var graphUrl = "/viewstat.jsp?stat=[I2PSnark] InBps&showEvents=false&" +
+var graphUrlBase = "/viewstat.jsp?stat=[I2PSnark] InBps&showEvents=false&" +
                "period=60000&periodCount=1440&end=0&width=2000&height=160&hideLegend=true&" +
-               "hideTitle=true&hideGrid=true&t=";
+               "hideTitle=true&hideGrid=true&";
+
+// track last refresh time
+var lastRefresh = 0;
 
 function refreshGraph() {
-  var now = new Date().getTime();
+  var now = Date.now();
+
+  // check if at least 5 minutes has elapsed since last refresh
+  if (now - lastRefresh < 5*60*1000) {
+    return;
+  }
+
+  // update last refresh time
+  lastRefresh = now;
+
+  // update graph URL with current timestamp
+  var graphUrl = graphUrlBase + "t=" + now;
 
   xhrGraph = new XMLHttpRequest();
-  xhrGraph.open("GET", graphUrl + now, true);
+  xhrGraph.open("GET", graphUrl, true);
   xhrGraph.onload = function() {
     if (xhrGraph.status == 200 && graphcss) {
-      graphcss.innerText = ":root{--snarkGraph:url('" + graphUrl + now + "')}";
+      graphcss.innerText = ":root{--snarkGraph:url('" + graphUrl + "')}";
     }
   };
   xhrGraph.send();
 
   function clearIntervalAndAbort() {
-    clearInterval(intervalId);
+    clearInterval(graphIntervalId);
     if (xhrGraph.readyState !== 4) {
       xhrGraph.abort();
     }
   }
 
-  var intervalId = setInterval(refreshGraph, noload ? 5*60*1000 : 15*60*1000);
+  if (!graphIntervalId) {
+    var graphIntervalId = setInterval(refreshGraph, noload ? 5*60*1000 : 15*60*1000);
+  }
   window.addEventListener('beforeunload', clearIntervalAndAbort);
 }
 
