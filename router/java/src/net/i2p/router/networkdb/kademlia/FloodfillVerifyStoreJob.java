1b89b001b076c247dfb66c8e7194ceab5022a010
dr|z3d <z3d@i2pmail.org>
2023-09-25 03:26:18 +0000

    Router: logging tweak

--- router/java/src/net/i2p/router/networkdb/kademlia/FloodfillVerifyStoreJob.java
+++ router/java/src/net/i2p/router/networkdb/kademlia/FloodfillVerifyStoreJob.java
@@ -341,11 +341,11 @@ class FloodfillVerifyStoreJob extends JobImpl {
                 boolean rv = _key.equals(dsrm.getSearchKey());
                 if (rv) {
                     if (_log.shouldInfo())
-                        _log.info("[JobId: " + getJobId() + "] DbSearchReplyMessage key match successful" +
+                        _log.info("[JobId: " + getJobId() + "] DbSearchReplyMsg key match successful" +
                                   "\n* DbId: " + _facade._dbid);
                 } else {
                     if (_log.shouldWarn())
-                        _log.warn("[JobId: " + getJobId() + "] DbSearchReplyMessage key mismatch for key [" + _key.toBase64().substring(0,6) +
+                        _log.warn("[JobId: " + getJobId() + "] DbSearchReplyMsg key mismatch for key [" + _key.toBase64().substring(0,6) +
                                   "] with DSRM " + message + "\n* DbId: " + _facade._dbid);
                 }
                 return rv;
