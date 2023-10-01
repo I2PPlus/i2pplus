f147bb17106cadeaceff9dd531bb30c88c223b49
dr|z3d <z3d@i2pmail.org>
2023-10-01 15:01:09 +0000

    I2PSnark: more fixes/optimizations for refreshTorrents.js

--- apps/i2psnark/res/js/refreshTorrents.js
+++ apps/i2psnark/res/js/refreshTorrents.js
@@ -33,13 +33,18 @@ function refreshTorrents(callback) {
   const storage = window.localStorage.getItem("snarkFilter");
   const torrents = document.getElementById("torrents");
 
-  if (xhrsnark.readyState !== XMLHttpRequest.UNSENT) {
-    xhrsnark.abort();
-  }
+  //if (xhrsnark.readyState !== XMLHttpRequest.UNSENT) {
+  //  xhrsnark.abort();
+  //  console.log("XHR request aborted");
+  //  requestInProgress = false;
+  //} else {
+  //  requestInProgress = true;
+  //}
 
   if (typeof callback === "function") {
     callback(xhrsnark.responseXML, getURL());
   }
+
 
   if (document.getElementById("ourDest") === null) {
     const childElems = document.querySelectorAll("#snarkHead th:not(.torrentAction)>*");
@@ -50,153 +55,161 @@ function refreshTorrents(callback) {
   }
 
   requestInProgress = true;
-  xhrsnark.onload = function () {
 
-    if (!storageRefresh) {
-      getRefreshInterval();
+  if (!storageRefresh) {
+    getRefreshInterval();
+  }
+
+  if (down || noload) {
+    window.requestAnimationFrame(refreshAll);
+  } else if (files || torrents) {
+    window.requestAnimationFrame(refreshHeaderAndFooter);
+    window.requestAnimationFrame(updateVolatile);
+  }
+
+  if (torrents?.responseXML) {
+      filterbar.removeEventListener("mouseover", setLinks);
+      const torrentsContainer = document.getElementById("torrents");
+      if (torrentsContainer?.responseXML) {
+        const newTorrents = xhrsnark.responseXML.getElementById("torrents");
+        if (newTorrents && torrentsContainer.innerHTML !== newTorrents.innerHTML) {
+          torrentsContainer.innerHTML = newTorrents.innerHTML;
+        }
+      }
+  } else if (dirlist?.responseXML) {
+    if (info) {
+      const infoResponse = xhrsnark.responseXML.getElementById("torrentInfoStats");
+      if (infoResponse) {
+        const infoParent = info.parentNode;
+        if (!Object.is(info.innerHTML, infoResponse.innerHTML)) {
+          infoParent.replaceChild(infoResponse, info);
+        }
+      }
+    }
+    if (control) {
+      const controlResponse = xhrsnark.responseXML.getElementById("torrentInfoControl");
+      if (controlResponse) {
+        const controlParent = control.parentNode;
+        if (!Object.is(control.innerHTML, controlResponse.innerHTML)) {
+          controlParent.replaceChild(controlResponse, control);
+        }
+      }
+    }
+    if (complete?.length && dirlist?.responseXML) {
+      const completeResponse = xhrsnark.responseXML.getElementsByClassName("completed");
+      for (let i = 0; i < complete.length && completeResponse?.length; i++) {
+        if (!Object.is(complete[i].innerHTML, completeResponse[i]?.innerHTML)) {
+          complete[i].innerHTML = completeResponse[i].innerHTML;
+        }
+      }
+    }
+    setLinks(query);
+  }
+
+  function refreshHeaderAndFooter() {
+    const snarkHead = document.getElementById("snarkHead");
+    const snarkHeadThs = document.querySelectorAll("#snarkHead th:not(.torrentLink)");
+    const snarkFoot = document.getElementById("snarkFoot");
+    const snarkFootThs = document.querySelectorAll("#snarkFoot th");
+    if (snarkHead?.responseXML) {
+      const snarkHeadResponse = xhrsnark.responseXML.getElementById("snarkHead");
+      const snarkHeadThsResponse = xhrsnark.responseXML.querySelectorAll("#snarkHead th:not(.torrentLink)");
+      if (snarkHead && snarkHeadResponse && !Object.is(snarkHead.innerHTML, snarkHeadResponse.innerHTML)) {
+        let updated = false;
+        Array.from(snarkHeadThs).forEach((elem, index) => {
+          if (elem.innerHTML !== snarkHeadThsResponse[index].innerHTML) {
+            elem.innerHTML = snarkHeadThsResponse[index].innerHTML;
+            updated = true;
+          }
+        });
+      }
+    }
+    if (snarkFoot?.responseXML) {
+      const snarkFootResponse = xhrsnark.responseXML.getElementById("snarkFoot");
+      const snarkFootThsResponse = xhrsnark.responseXML.querySelectorAll("#snarkFoot th");
+      if (snarkFoot && snarkFootResponse && !Object.is(snarkFoot.innerHTML, snarkFootResponse.innerHTML)) {
+        let updated = false;
+        Array.from(snarkFootThs).forEach((elem, index) => {
+          if (elem.innerHTML !== snarkFootThsResponse[index].innerHTML) {
+            elem.innerHTML = snarkFootThsResponse[index].innerHTML;
+            updated = true;
+          }
+        });
+      }
+    }
+  }
+
+  function refreshAll() {
+    const files = document.getElementById("dirInfo");
+    const mainsection = document.getElementById("mainsection");
+    const notfound = document.getElementById("NotFound");
+    const dirlist = document.getElementById("dirlist");
+    if (mainsection) {
+      const mainsectionResponse = xhrsnark.responseXML?.getElementById("mainsection");
+      if (mainsectionResponse && mainsection.innerHTML !== mainsectionResponse.innerHTML) {
+        mainsection.innerHTML = mainsectionResponse.innerHTML;
+      }
+      if (filterbar) {
+        initFilterBar();
+      }
+    } else if (files) {
+      const dirlistResponse = xhrsnark.responseXML?.getElementById("dirlist");
+      if (dirlistResponse && !Object.is(dirlist.innerHTML, dirlistResponse.innerHTML) && notfound === null) {
+        dirlist.innerHTML = dirlistResponse.innerHTML;
+      }
+    }
+  }
+
+  function updateVolatile() {
+    const dirlist = document.getElementById("dirlist");
+    const snarkTr = document.querySelectorAll("tr.volatile");
+    const updating = document.querySelectorAll("tr.volatile, #messages");
+    const updatingResponse = xhrsnark.responseXML?.querySelectorAll("tr.volatile, #messages");
+
+    let updated = false;
+
+    if (updatingResponse && updatingResponse.length && Array.from(updating).every(function (elem) {
+      return Array.from(updatingResponse).includes(elem);
+    })) {
+      console.log("Already updating");
+      updated = false;
     }
 
-    if (down || noload) {
-      window.requestAnimationFrame(refreshAll);
-    } else if (files || torrents) {
+    for (let i = 0; i < updating.length; i++) {
+      const updatingTds = updating[i].querySelectorAll("td:not(.details.data):not(.magnet):not(.trackerLink)");
+      const updatingTdsResponse = updatingResponse[i]?.querySelectorAll("td:not(.details.data):not(.magnet):not(.trackerLink)");
+
+      if (updatingResponse && updating[i].innerHTML !== updatingResponse[i]?.innerHTML) {
+        if (updating[i].classList.contains("even")) {
+          updating[i].outerHTML = updatingResponse[i].outerHTML;
+        } else {
+          updating[i].outerHTML = updatingResponse[i].outerHTML.replace(/(<tr class="[^"]*)even([^"]*">)/, "$1$2");
+        }
+        updated = true;
+      } else if (updatingResponse) {
+        for (let j = 0; j < updatingTds.length; j++) {
+          updatingTds[j].innerHTML = updatingTdsResponse[j].innerHTML;
+          updated = true;
+        }
+      }
+    }
+
+    if (updated) {
       window.requestAnimationFrame(refreshHeaderAndFooter);
-      window.requestAnimationFrame(updateVolatile);
+    } else if (requestInProgress) {
+      xhrsnark.abort();
+      console.log("XHR request aborted");
+      window.requestAnimationFrame(refreshAll);
     }
+  }
 
-    if (torrents?.responseXML) {
-        filterbar.removeEventListener("mouseover", setLinks);
-        const torrentsContainer = document.getElementById("torrents");
-        if (torrentsContainer?.responseXML) {
-          const newTorrents = xhrsnark.responseXML.getElementById("torrents");
-          if (newTorrents && torrentsContainer.innerHTML !== newTorrents.innerHTML) {
-            torrentsContainer.innerHTML = newTorrents.innerHTML;
-          }
-        }
-    } else if (dirlist?.responseXML) {
-      if (info) {
-        const infoResponse = xhrsnark.responseXML.getElementById("torrentInfoStats");
-        if (infoResponse) {
-          const infoParent = info.parentNode;
-          if (!Object.is(info.innerHTML, infoResponse.innerHTML)) {
-            infoParent.replaceChild(infoResponse, info);
-          }
-        }
-      }
-      if (control) {
-        const controlResponse = xhrsnark.responseXML.getElementById("torrentInfoControl");
-        if (controlResponse) {
-          const controlParent = control.parentNode;
-          if (!Object.is(control.innerHTML, controlResponse.innerHTML)) {
-            controlParent.replaceChild(controlResponse, control);
-          }
-        }
-      }
-      if (complete?.length && dirlist?.responseXML) {
-        const completeResponse = xhrsnark.responseXML.getElementsByClassName("completed");
-        for (let i = 0; i < complete.length && completeResponse?.length; i++) {
-          if (!Object.is(complete[i].innerHTML, completeResponse[i]?.innerHTML)) {
-            complete[i].innerHTML = completeResponse[i].innerHTML;
-          }
-        }
-      }
-      setLinks(query);
-    }
+  function getRefreshInterval() {
+    const interval = parseInt(xhrsnark.getResponseHeader("X-Snark-Refresh-Interval")) || 5;
+    localStorage.setItem("snarkRefresh", interval);
+    return interval * 1000;
+  }
 
-    function refreshHeaderAndFooter() {
-      const snarkHead = document.getElementById("snarkHead");
-      const snarkHeadThs = document.querySelectorAll("#snarkHead th:not(.torrentLink)");
-      const snarkFoot = document.getElementById("snarkFoot");
-      const snarkFootThs = document.querySelectorAll("#snarkFoot th");
-      if (snarkHead?.responseXML) {
-        const snarkHeadResponse = xhrsnark.responseXML.getElementById("snarkHead");
-        const snarkHeadThsResponse = xhrsnark.responseXML.querySelectorAll("#snarkHead th:not(.torrentLink)");
-        if (snarkHead && snarkHeadResponse && !Object.is(snarkHead.innerHTML, snarkHeadResponse.innerHTML)) {
-          let updated = false;
-          Array.from(snarkHeadThs).forEach((elem, index) => {
-            if (elem.innerHTML !== snarkHeadThsResponse[index].innerHTML) {
-              elem.innerHTML = snarkHeadThsResponse[index].innerHTML;
-              updated = true;
-            }
-          });
-        }
-      }
-      if (snarkFoot?.responseXML) {
-        const snarkFootResponse = xhrsnark.responseXML.getElementById("snarkFoot");
-        const snarkFootThsResponse = xhrsnark.responseXML.querySelectorAll("#snarkFoot th");
-        if (snarkFoot && snarkFootResponse && !Object.is(snarkFoot.innerHTML, snarkFootResponse.innerHTML)) {
-          let updated = false;
-          Array.from(snarkFootThs).forEach((elem, index) => {
-            if (elem.innerHTML !== snarkFootThsResponse[index].innerHTML) {
-              elem.innerHTML = snarkFootThsResponse[index].innerHTML;
-              updated = true;
-            }
-          });
-        }
-      }
-    }
-
-    function refreshAll() {
-      const files = document.getElementById("dirInfo");
-      const mainsection = document.getElementById("mainsection");
-      const notfound = document.getElementById("NotFound");
-      const dirlist = document.getElementById("dirlist");
-      if (mainsection) {
-        const mainsectionResponse = xhrsnark.responseXML?.getElementById("mainsection");
-        if (mainsectionResponse && mainsection.innerHTML !== mainsectionResponse.innerHTML) {
-          mainsection.innerHTML = mainsectionResponse.innerHTML;
-        }
-        if (filterbar) {
-          initFilterBar();
-        }
-      } else if (files) {
-        const dirlistResponse = xhrsnark.responseXML?.getElementById("dirlist");
-        if (dirlistResponse && !Object.is(dirlist.innerHTML, dirlistResponse.innerHTML) && notfound === null) {
-          dirlist.innerHTML = dirlistResponse.innerHTML;
-        }
-      }
-    }
-
-    function updateVolatile() {
-      const dirlist = document.getElementById("dirlist");
-      const snarkTr = document.querySelectorAll("tr.volatile");
-      const snarkTrResponse = xhrsnark.responseXML?.querySelectorAll("tr.volatile");
-      const updating = document.querySelectorAll("tr.volatile, #messages");
-      const updatingResponse = xhrsnark.responseXML?.querySelectorAll("tr.volatile, #messages");
-      const updatingTds = document.querySelectorAll(".volatile td:not(.details.data):not(.magnet):not(.trackerLink)");
-      const updatingTdsResponse = xhrsnark.responseXML?.querySelectorAll(".volatile td:not(.details.data):not(.magnet):not(.trackerLink)");
-      let updated = false;
-
-      if (updatingResponse?.length && updating.length === updatingResponse.length) {
-        Array.from(updatingTds).forEach((elem, index) => {
-          if (updatingResponse[index] && elem.innerHTML !== updatingTdsResponse[index].innerHTML) {
-            elem.innerHTML = updatingTdsResponse[index].innerHTML;
-            updated = true;
-          }
-        });
-        Array.from(updating).forEach((elem, index) => {
-          if (updatingResponse[index] && elem.innerHTML !== updatingResponse[index].innerHTML) {
-            const responseElem = updatingResponse[index];
-            elem.outerHTML = updatingResponse[index].outerHTML;
-            updated = true;
-          }
-        });
-        window.requestAnimationFrame(refreshHeaderAndFooter);
-      } else {
-        window.requestAnimationFrame(refreshAll);
-      }
-    }
-
-    if (typeof callback === "function") {
-      callback(xhrsnark.responseXML, getURL());
-    }
-
-    function getRefreshInterval() {
-      const interval = parseInt(xhrsnark.getResponseHeader("X-Snark-Refresh-Interval")) || 5;
-      localStorage.setItem("snarkRefresh", interval);
-      return interval * 1000;
-    }
-    requestInProgress = false;
-  };
+  requestInProgress = false;
 
   if (filterbar) {
     initFilterBar();
@@ -207,7 +220,7 @@ function refreshTorrents(callback) {
     if (refreshTimeoutId) {
       clearTimeout(refreshTimeoutId);
     }
-    refreshTimeoutId = setTimeout(() => refreshTorrents(initFunctions), 1000);
+    refreshTimeoutId = setTimeout(() => refreshTorrents(initHandlers), 1000);
     requestInProgress = false;
   };
 
@@ -220,11 +233,13 @@ function getURL() {
   return url;
 }
 
-function initFunctions() {
+function initHandlers() {
   setLinks();
   initLinkToggler();
   magnetToast();
   attachMagnetListeners();
+  const screenlog = document.getElementById("screenlog");
+  if (screenlog) {initToggleLog();}
 }
 
 function setLinks(query) {
@@ -248,18 +263,11 @@ function noAjax(delay) {
   }, delay);
 }
 
-async function initSnarkRefresh() {
+async function initSnarkRefresh(initHandlers) {
   const interval = (parseInt(storageRefresh) || 5) * 1000;
   clearInterval(refreshIntervalId);
   refreshIntervalId = setInterval(async () => {
     await doRefresh();
-    setLinks();
-    initLinkToggler();
-    magnetToast();
-    attachMagnetListeners();
-    initSnarkAlert();
-    const screenlog = document.getElementById("screenlog");
-    if (screenlog) {initToggleLog();}
   }, interval);
   if (files && document.getElementById("lightbox")) {
     const lightbox = new Lightbox();
@@ -268,13 +276,12 @@ async function initSnarkRefresh() {
 }
 
 async function doRefresh() {
-  const xhr = new XMLHttpRequest();
-  xhrsnark.responseType = "document";
   xhrsnark.open("GET", getURL(), true);
+  xhrsnark.responseType = "document";
   xhrsnark.send();
   await new Promise((resolve, reject) => {
     xhrsnark.onload = () => {
-      refreshTorrents(xhrsnark);
+      refreshTorrents(initHandlers);
       resolve();
     };
     xhrsnark.onerror = () => {
