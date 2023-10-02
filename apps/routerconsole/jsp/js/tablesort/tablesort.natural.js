a2aee57c9bbbc9464a1b55fddf00237c68cc37c3
dr|z3d <z3d@i2pmail.org>
2023-10-02 07:07:55 +0000

    Console: optimize natural tablesort script, place null/unknown returns at end of sort

--- apps/routerconsole/jsp/js/tablesort/tablesort.natural.js
+++ apps/routerconsole/jsp/js/tablesort/tablesort.natural.js
@@ -2,24 +2,48 @@
 // https://github.com/etrinh/tablesort/blob/6a47651de804240115d43c1b48156c6b1ebe096a/src/sorts/tablesort.natural.js
 // https://stackoverflow.com/questions/2802341/javascript-natural-sort-of-alphanumerical-strings
 
-(function(){
-  Tablesort.extend('natural', item => true, function(as, bs) {
-    let a, b, a1, b1, i = 0, n, L,
-        rx = /(\.\d+)|(\d+(\.\d+)?)|([^\d.]+)|(\.\D+)|(\.$)/g;
-    if (as === bs) return 0;
-    a = as.toLowerCase().match(rx);
-    b = bs.toLowerCase().match(rx);
-    L = a.length;
-    while (i < L) {
-        if (!b[i]) return -1;
-        a1 = a[i],
-            b1 = b[i++];
-        if (a1 !== b1) {
-            n = a1 - b1;
-            if (!isNaN(n)) return -n;
-            return a1 > b1 ? -1 : 1;
-        }
+(function () {
+  Tablesort.extend('natural', function (item) {
+    return item !== null && item.trim() !== '';
+  }, function (a, b) {
+    if (a === b) {
+      return 0;
     }
-    return b[i] ? 1 : 0;
+    if (a === 'unknown' && b !== 'unknown') {
+      return 1;
+    }
+    if (b === 'unknown' && a !== 'unknown') {
+      return -1;
+    }
+    if (a === null && b !== null) {
+      return 1;
+    }
+    if (b === null && a !== null) {
+      return -1;
+    }
+    let aParts = a.toLowerCase().match(/(\.\d+)|(\d+(\.\d+)?)|([^\d.]+)/g);
+    let bParts = b.toLowerCase().match(/(\.\d+)|(\d+(\.\d+)?)|([^\d.]+)/g);
+    if (!bParts) {
+      return -1;
+    }
+    let aLength = aParts?.length;
+    let bLength = bParts?.length;
+    let minLength = Math.min(aLength, bLength);
+    for (let i = 0; i < minLength; i++) {
+      let aPart = aParts[i];
+      let bPart = bParts[i];
+      let aNum = parseFloat(aPart);
+      let bNum = parseFloat(bPart);
+      if (!isNaN(aNum) && !isNaN(bNum)) {
+        if (aNum !== bNum) {
+          return bNum - aNum;
+        }
+      } else {
+        if (aPart !== bPart) {
+          return aPart < bPart ? -1 : 1;
+        }
+      }
+    }
+    return bLength - aLength;
   });
-}());
+})();
