2c450fc12dd333e26d4230ea0309b5b405b7cd43
dr|z3d <z3d@i2pmail.org>
2023-10-22 11:02:52 +0000

    Console: TunnelBandwidthHelper

---
+++ apps/routerconsole/java/src/net/i2p/router/web/helpers/TunnelBandwidthHelper.java
@@ -0,0 +1,25 @@
+package net.i2p.router.web.helpers;
+
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+import net.i2p.router.RouterContext;
+import net.i2p.router.tunnel.pool.TunnelPool;
+import net.i2p.router.web.HelperBase;
+
+public class TunnelBandwidthHelper extends HelperBase {
+    public String getTunnelBandwidth() {
+        TunnelRenderer renderer = new TunnelRenderer(_context);
+        try {
+            StringWriter sw = new StringWriter(64*1024);
+            TunnelPool in = null;
+            TunnelPool out = null; // or pass actual TunnelPool objects if available
+            renderer.renderLifetimeBandwidth(sw, in, out);
+            return sw.toString();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            return "";
+        }
+    }
+}
