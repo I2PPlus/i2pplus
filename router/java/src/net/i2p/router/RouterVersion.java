568a21e5b52774572c9e2224af3ef47126bc0596
dr|z3d <z3d@i2pmail.org>
2023-10-23 22:56:08 +0000

    bump to -70+

--- router/java/src/net/i2p/router/RouterVersion.java
+++ router/java/src/net/i2p/router/RouterVersion.java
@@ -20,7 +20,7 @@ public class RouterVersion {
     public final static String VERSION = CoreVersion.VERSION;
     /** for example: "beta", "alpha", "rc" */
     public final static String STATUS = "";
-    public final static long BUILD = 69;
+    public final static long BUILD = 70;
 
     /** for example "-test" */
     public final static String EXTRA = "+";
