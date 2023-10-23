94eb1def65d0756af2dc5397551006e957c9fe41
dr|z3d <z3d@i2pmail.org>
2023-10-23 18:18:12 +0000

    Console: tweak refreshSidebar.js

--- apps/routerconsole/jsp/js/refreshSidebar.js
+++ apps/routerconsole/jsp/js/refreshSidebar.js
@@ -74,7 +74,7 @@ function tangoDown() {
     localtunnelSummary.innerHTML = "<tr id=routerdown><td colspan=3 height=10></td></tr>";
   }
   isDown = true;
-  refreshAll();
+  setTimeout(refreshSidebar, 3000);
 }
 
 function refreshSidebar() {
