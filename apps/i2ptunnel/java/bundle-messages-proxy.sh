9db4243a66d000e2fc8e11ad2225aa23e788971b
dr|z3d <z3d@i2pmail.org>
2023-11-01 15:40:33 +0000

    I2PTUnnel: translation bundle script fixup

--- apps/i2ptunnel/java/bundle-messages-proxy.sh
+++ apps/i2ptunnel/java/bundle-messages-proxy.sh
@@ -71,7 +71,7 @@ do
 		# To start a new translation, copy the header from an old translation to the new .po file,
 		# then ant distclean updater.
 		find $JPATHS -name *.java > $TMPFILE
-		xgettext -f $TMPFILE -F -L java --from-code=UTF-8 --add-comments\
+		xgettext -f $TMPFILE -F -L java --from-code=UTF-8 --no-wrap --add-comments\
 	                 --keyword=_t \
 		         -o ${i}t
 		if [ $? -ne 0 ]
@@ -104,7 +104,7 @@ do
         then
             # slow way
             # convert to class files in build/obj
-            msgfmt --java2 --no-wrap --no-fuzzy-matching --statistics -r $CLASS -l $LG -d build/obj $i
+            msgfmt --java2 --statistics -r $CLASS -l $LG -d build/obj $i
             if [ $? -ne 0 ]
             then
                 echo "ERROR - msgfmt failed on ${i}, not updating translations"
