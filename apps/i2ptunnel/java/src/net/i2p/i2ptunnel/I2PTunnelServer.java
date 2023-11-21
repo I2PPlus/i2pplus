4041df1454854cd6d37f89087c130de9f0cf5ff8
zzz <zzz@i2pmail.org>
2023-11-21 08:50:34 -0500

    i2ptunnel: Add thread number to server thread names
    like we already do for clients, to make debugging easier
    
    (cherry picked from commit 761967ed4b921ac23cd28340896d9879aa8a4683)

--- apps/i2ptunnel/java/src/net/i2p/i2ptunnel/I2PTunnelServer.java
+++ apps/i2ptunnel/java/src/net/i2p/i2ptunnel/I2PTunnelServer.java
@@ -28,6 +28,7 @@ import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicLong;
 
 import net.i2p.client.I2PClient;
 import net.i2p.client.I2PSession;
@@ -742,6 +743,7 @@ public class I2PTunnelServer extends I2PTunnelTask implements Runnable {
     /** just to set the name and set Daemon */
     private static class CustomThreadFactory implements ThreadFactory {
         private final String _name;
+        private final AtomicLong _executorThreadCount = new AtomicLong();
 
         public CustomThreadFactory(String name) {
             _name = name;
@@ -749,7 +751,7 @@ public class I2PTunnelServer extends I2PTunnelTask implements Runnable {
 
         public Thread newThread(Runnable r) {
             Thread rv = Executors.defaultThreadFactory().newThread(r);
-            rv.setName(_name);
+            rv.setName(_name + "[" + _executorThreadCount.incrementAndGet() + "]");
             rv.setPriority(Thread.MAX_PRIORITY);
             rv.setDaemon(true);
             return rv;
