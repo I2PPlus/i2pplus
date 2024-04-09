b02c77d2e9aa5e4f760156619a96db78acca2f6d
dr|z3d <z3d@i2pmail.org>
2024-04-09 02:46:58 +0000

    Console: add null detection to onVisible.js

--- apps/routerconsole/jsp/js/onVisible.js
+++ apps/routerconsole/jsp/js/onVisible.js
@@ -1,4 +1,8 @@
 function onVisible(element, callback) {
+  if (!element || !(element instanceof Element)) {
+    return;
+  }
+
   new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
       if (entry.intersectionRatio > 0 && !document.hidden) {
