9f9f15e3b8e9d299aa14d94c3e2d5485b0d0fc99
dr|z3d <z3d@i2pmail.org>
2024-04-27 13:42:40 +0000

    maketorblocklst.sh

---
+++ installer/resources/maketorblocklist.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Download https://check.torproject.org/torbulkexitlist
+# and then run this script in the same folder to generate
+# a consolidated, sorted list
+
+# Specify input and output file variables
+input_file="torbulkexitlist"
+output_file="tor-blocklist.txt"
+
+# Check if the input file exists
+if [ ! -f $input_file ]; then
+  echo "Input file does not exist or is not a regular file." >> $log_file
+  exit 1
+fi
+
+# Sort the list of IPs naturally
+sorted_ips=$(cat $input_file | sort -V)
+
+# Initialize variables
+start_ip=""
+end_ip=""
+output=""
+
+# Function to print the IP range
+print_range() {
+  if [ "$start_ip" = "$end_ip" ]; then
+    output+="$start_ip\n"
+  else
+    output+="$start_ip-$end_ip\n"
+  fi
+}
+
+# Loop through the sorted IPs to find consecutive IPs
+for ip in $sorted_ips; do
+  if [ "$start_ip" = "" ]; then
+    start_ip=$ip
+    end_ip=$ip
+  elif [ $(expr $(cut -d\. -f4 <<< $ip) - $(cut -d\. -f4 <<< $end_ip)) -eq 1 ]; then
+    end_ip=$ip
+  else
+    print_range
+    start_ip=$ip
+    end_ip=$ip
+  fi
+done
+
+# Print the last range
+print_range
+
+# Output the consolidated IPs to file
+echo "Consolidated IPs saved to: $output_file"
+echo -e "$output" > $output_file
