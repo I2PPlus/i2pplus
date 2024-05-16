9a2d33c828fd55e8ad6fcdaff4de77ba1018927e
zzz <zzz@i2pmail.org>
2024-05-16 09:21:24 -0400

    Util: Remove ST2 timers from queue on cancel
    May reduce memory usage a little
    
    (cherry picked from commit 6f3bc3f5e2f5024c8d98a2fb3b5271b4f86c5519)

--- core/java/src/net/i2p/util/SimpleTimer2.java
+++ core/java/src/net/i2p/util/SimpleTimer2.java
@@ -120,6 +120,7 @@ public class SimpleTimer2 {
     private static class CustomScheduledThreadPoolExecutor extends ScheduledThreadPoolExecutor {
         public CustomScheduledThreadPoolExecutor(int threads, ThreadFactory factory) {
              super(threads, factory);
+             setRemoveOnCancelPolicy(true);
         }
 
         @Override
