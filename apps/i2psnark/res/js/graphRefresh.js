7d3c124ae04f6ebac6288a2cb547586af5b18704
dr|z3d <z3d@i2pmail.org>
2024-05-19 03:21:49 +0000

    I2PSnark: fixup messagelog graph aspect ratio; graphRefresh.js optimizations

--- apps/i2psnark/res/js/graphRefresh.js
+++ apps/i2psnark/res/js/graphRefresh.js
@@ -2,50 +2,42 @@
 /* Update snark download graph dynamically and apply to message log background if available */
 /* License: AGPL3 or later */
 
-var graphcss = document.getElementById("graphcss");
-var noload = document.getElementById("noload");
-var xhrGraph;
-var graphUrlBase = "/viewstat.jsp?stat=[I2PSnark] InBps&showEvents=false&" +
-               "period=60000&periodCount=1440&end=0&width=2000&height=160&hideLegend=true&" +
-               "hideTitle=true&hideGrid=true&";
-
 // track last refresh time
-var lastRefresh = 0;
+let lastSnarkGraphRefresh = 0;
 
 function refreshGraph() {
-  var now = Date.now();
+  const now = Date.now();
+  const graphcss = document.getElementById("graphcss");
+  const noload = document.getElementById("noload");
+  const graphUrlBase = "/viewstat.jsp?stat=[I2PSnark] InBps&showEvents=false&" +
+                       "period=60000&periodCount=1440&end=0&width=2000&height=160&hideLegend=true&" +
+                       "hideTitle=true&hideGrid=true&";
 
   // check if at least 5 minutes has elapsed since last refresh
-  if (now - lastRefresh < 5*60*1000) {
-    return;
-  }
+  if (now - lastRefresh < 5*60*1000) {return;}
 
   // update last refresh time
-  lastRefresh = now;
+  lastSnarkGraphRefresh = now;
 
   // update graph URL with current timestamp
   var graphUrl = graphUrlBase + "t=" + now;
 
-  xhrGraph = new XMLHttpRequest();
-  xhrGraph.open("GET", graphUrl, true);
-  xhrGraph.onload = function() {
-    if (xhrGraph.status == 200 && graphcss) {
+  const xhrSnarkGraph = new XMLHttpRequest();
+  xhrSnarkGraph.open("GET", graphUrl, true);
+  xhrSnarkGraph.onload = function() {
+    if (xhrSnarkGraph.status == 200 && graphcss) {
       graphcss.innerText = ":root{--snarkGraph:url('" + graphUrl + "')}";
     }
   };
-  xhrGraph.send();
+  xhrSnarkGraph.send();
 
   function clearIntervalAndAbort() {
-    clearInterval(graphIntervalId);
-    if (xhrGraph.readyState !== 4) {
-      xhrGraph.abort();
-    }
+    clearInterval(snarkGraphIntervalId);
+    if (xhrSnarkGraph.readyState !== 4) {xhrSnarkGraph.abort();}
   }
 
-  if (!graphIntervalId) {
-    var graphIntervalId = setInterval(refreshGraph, noload ? 5*60*1000 : 15*60*1000);
-  }
-  window.addEventListener('beforeunload', clearIntervalAndAbort);
+  if (!snarkGraphIntervalId) {var snarkGraphIntervalId = setInterval(refreshGraph, noload ? 5*60*1000 : 15*60*1000);}
+  window.addEventListener("beforeunload", clearIntervalAndAbort);
 }
 
 document.addEventListener("DOMContentLoaded", refreshGraph);
