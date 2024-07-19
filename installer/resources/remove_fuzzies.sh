2356c0f0971db7f835d4ab0298de8bf1d013fdb2
dr|z3d <z3d@i2pmail.org>
2024-07-19 12:32:20 +0000

    Scripts: add optional --no-location flag to remove_fuzzies

--- installer/resources/remove_fuzzies.sh
+++ installer/resources/remove_fuzzies.sh
@@ -1,27 +1,35 @@
 #!/bin/sh
 
-# Recursively remove fuzzy translations from .po files with an option to prevent line wrapping
-# Usage: remove_fuzzy_translations.sh <directory> [--no-wrap]
+# Recursively remove fuzzy translations from .po files with options to prevent line wrapping and remove code location comments
+# Usage: remove_fuzzy_translations.sh <directory> [--no-wrap] [--no-location]
 
 # Initialize counters
 num_files=0
 num_fuzzy=0
 no_wrap=0
+no_location=0
 
 # Check and parse arguments
-if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
+if [ "$#" -lt 1 ] || [ "$#" -gt 3 ]; then
     echo " ! Error: Incorrect number of arguments."
-    echo " > Usage: $0 <directory> [--no-wrap]"
+    echo " > Usage: $0 <directory> [--no-wrap] [--no-location]"
     echo " > This script removes fuzzy translations from .po files."
     echo " > Optional '--no-wrap' flag removes line wrapping in .po files."
+    echo " > Optional '--no-location' flag removes code location comments in .po files."
     exit 1
 fi
 
 directory="$1"
 
 # Check for --no-wrap argument
-if [ "$#" -eq 2 ] && [ "$2" = "--no-wrap" ]; then
+if [ "$#" -ge 2 ] && [ "$2" = "--no-wrap" ]; then
     no_wrap=1
+    shift
+fi
+
+# Check for --no-location argument
+if [ "$#" -ge 2 ] && [ "$2" = "--no-location" ]; then
+    no_location=1
 fi
 
 # Create a temporary file to hold the names of the .po files
@@ -37,11 +45,19 @@ while IFS= read -r po_file; do
   num_fuzzy_entries=$(grep -c '^#, fuzzy' "$po_file")
   num_fuzzy=$((num_fuzzy + num_fuzzy_entries))
 
-  # Define the msgattrib command with or without --no-wrap
+  # Define the msgattrib command with or without --no-wrap and --no-location
   if [ $no_wrap -eq 1 ]; then
-    msgattrib_cmd="msgattrib --no-fuzzy --no-wrap --output-file='${po_file}' '${po_file}'"
+    msgattrib_cmd="msgattrib --no-fuzzy"
+    if [ $no_location -eq 1 ]; then
+      msgattrib_cmd="$msgattrib_cmd --no-location"
+    fi
+    msgattrib_cmd="$msgattrib_cmd --no-wrap --output-file='${po_file}' '${po_file}'"
   else
-    msgattrib_cmd="msgattrib --no-fuzzy --output-file='${po_file}' '${po_file}'"
+    msgattrib_cmd="msgattrib --no-fuzzy"
+    if [ $no_location -eq 1 ]; then
+      msgattrib_cmd="$msgattrib_cmd --no-location"
+    fi
+    msgattrib_cmd="$msgattrib_cmd --output-file='${po_file}' '${po_file}'"
   fi
 
   # Execute the msgattrib command
@@ -59,4 +75,7 @@ echo " > Processed $num_files .po files."
 echo " > Removed $num_fuzzy fuzzy translations."
 if [ $no_wrap -eq 1 ]; then
   echo " > Any line wrapping found in .po files has been removed."
+fi
+if [ $no_location -eq 1 ]; then
+  echo " > Code location comments have been removed from .po files."
 fi
