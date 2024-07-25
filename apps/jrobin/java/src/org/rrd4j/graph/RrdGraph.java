838f0818a1c8048ebf18b57cba3ce1bade25bda5
zzz <zzz@i2pmail.org>
2024-07-25 08:08:24 -0400

    rrd4j: More optimizations for graph generation
    (cherry picked from commit 6aa6dad5be502643690b5d10c7c109ce6192dbf9)
    (cherry picked from commit 0468788eefeb79cad044b4ac008c94ebc8cff1cd)

--- apps/jrobin/java/src/org/rrd4j/graph/RrdGraph.java
+++ apps/jrobin/java/src/org/rrd4j/graph/RrdGraph.java
@@ -68,6 +68,12 @@ public class RrdGraph implements RrdGraphConstants {
         }
     }
 
+    /**
+     * Create graph from a custom image worker
+     * @param gdef
+     * @param worker
+     * @throws IOException
+     */
     public RrdGraph(RrdGraphDef gdef, ImageWorker worker) throws IOException {
         this.gdef = gdef;
         signature = gdef.getSignature();
@@ -140,8 +146,7 @@ public class RrdGraph implements RrdGraphConstants {
                 drawAxis();
                 drawText();
                 drawLegend();
-                drawRules();
-                drawSpans();
+                drawRulesAndSpans();
                 gator();
                 drawOverlay();
                 saveImage();
@@ -213,13 +218,17 @@ public class RrdGraph implements RrdGraphConstants {
         }
     }
 
-    private void drawRules() {
-        worker.clip(im.xorigin + 1, im.yorigin - gdef.height - 1, gdef.width - 1, gdef.height + 2);
+    private void drawRulesAndSpans() {
+        boolean found = false;
         for (PlotElement pe : gdef.plotElements) {
             if (pe instanceof HRule) {
                 HRule hr = (HRule) pe;
                 if (hr.value >= im.minval && hr.value <= im.maxval) {
                     int y = mapper.ytr(hr.value);
+                    if (!found) {
+                        worker.clip(im.xorigin + 1, im.yorigin - gdef.height - 1, gdef.width - 1, gdef.height + 2);
+                        found = true;
+                    }
                     worker.drawLine(im.xorigin, y, im.xorigin + im.xsize, y, hr.color, hr.stroke);
                 }
             }
@@ -227,43 +236,47 @@ public class RrdGraph implements RrdGraphConstants {
                 VRule vr = (VRule) pe;
                 if (vr.timestamp >= im.start && vr.timestamp <= im.end) {
                     int x = mapper.xtr(vr.timestamp);
-                    if (im.xsize >= 600 && im.ysize >= 200) {
-                        worker.drawLine(x, im.yorigin - 1, x, im.yorigin - im.ysize + 1, vr.color, vr.stroke);
-                    } else {
-                        worker.drawLine(x, im.yorigin, x, im.yorigin - im.ysize + 1, vr.color, vr.stroke);
+                    if (!found) {
+                        worker.clip(im.xorigin + 1, im.yorigin - gdef.height - 1, gdef.width - 1, gdef.height + 2);
+                        found = true;
                     }
+                    worker.drawLine(x, im.yorigin, x, im.yorigin - im.ysize, vr.color, vr.stroke);
                 }
             }
-        }
-        worker.reset();
-    }
-
-    private void drawSpans() {
-        worker.clip(im.xorigin + 1, im.yorigin - gdef.height - 1, gdef.width - 1, gdef.height + 2);
-        for (PlotElement pe : gdef.plotElements) {
-            if (pe instanceof HSpan) {
+            else if (pe instanceof HSpan) {
                 HSpan hr = (HSpan) pe;
                 int ys = mapper.ytr(hr.start);
                 int ye = mapper.ytr(hr.end);
                 int height = ys - ye;
+                if (!found) {
+                    worker.clip(im.xorigin + 1, im.yorigin - gdef.height - 1, gdef.width - 1, gdef.height + 2);
+                    found = true;
+                }
                 worker.fillRect(im.xorigin, ys - height, im.xsize, height, hr.color);
             }
             else if (pe instanceof VSpan) {
                 VSpan vr = (VSpan) pe;
                 int xs = mapper.xtr(vr.start);
                 int xe = mapper.xtr(vr.end);
+                if (!found) {
+                    worker.clip(im.xorigin + 1, im.yorigin - gdef.height - 1, gdef.width - 1, gdef.height + 2);
+                    found = true;
+                }
                 worker.fillRect(xs, im.yorigin - im.ysize, xe - xs, im.ysize, vr.color);
             }
         }
-        worker.reset();
+        if (found)
+            worker.reset();
     }
 
     private void drawText() {
         if (!gdef.onlyGraph) {
             worker.setTextAntiAliasing(gdef.textAntiAliasing);
             if (gdef.title != null) {
-                int x = im.xgif / 2 - (int) (worker.getStringWidth(gdef.title, gdef.getFont(FONTTAG_TITLE)) / 2);
-                int y = PADDING_TOP + (int) worker.getFontAscent(gdef.getFont(FONTTAG_TITLE));
+                // I2P truncate on the right only
+                int x = Math.max(2, im.xgif / 2 - (int) (worker.getStringWidth(gdef.title, gdef.getFont(FONTTAG_TITLE)) / 2));
+                // I2P a little less padding on top and more on the bottom
+                int y = PADDING_TOP * 2 / 3 + (int) worker.getFontAscent(gdef.getFont(FONTTAG_TITLE));
                 worker.drawString(gdef.title, x, y, gdef.getFont(FONTTAG_TITLE), gdef.getColor(ElementsNames.font));
             }
             if (gdef.verticalLabel != null) {
@@ -685,9 +698,15 @@ public class RrdGraph implements RrdGraphConstants {
                     if (c instanceof LegendText) {
                         // draw with BOX
                         worker.fillRect(x, y - box, box, box, gdef.getColor(ElementsNames.frame));
+                        Color bc = (Color) gdef.getColor(ElementsNames.back);
+                        Color lc = (Color) ((LegendText) c).legendColor;
+                        // no use drawing unless both the two on top have some transparency
+                        if (bc.getAlpha() < 255 && lc.getAlpha() < 255)
                         worker.fillRect(x + 1, y - box + 1, box - 2, box - 2, gdef.getColor(ElementsNames.canvas));
-                        worker.fillRect(x + 1, y - box + 1, box - 2, box - 2, gdef.getColor(ElementsNames.back));
-                        worker.fillRect(x + 1, y - box + 1, box - 2, box - 2, ((LegendText) c).legendColor);
+                        // no use drawing unless the one on top has some transparency
+                        if (lc.getAlpha() < 255)
+                            worker.fillRect(x + 1, y - box + 1, box - 2, box - 2, bc);
+                        worker.fillRect(x + 1, y - box + 1, box - 2, box - 2, lc);
                         worker.drawString(c.resolvedText, x + boxSpace, y, gdef.getFont(FONTTAG_LEGEND), gdef.getColor(ElementsNames.font));
                     }
                     else {
