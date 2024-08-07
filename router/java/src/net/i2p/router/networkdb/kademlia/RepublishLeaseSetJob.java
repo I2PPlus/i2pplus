c7e5127f7ecdc12c296686a647b9beb30bd39dd3
dr|z3d <z3d@i2pmail.org>
2024-08-07 15:15:07 +0000

    Router: increase period for republishing LS

--- router/java/src/net/i2p/router/networkdb/kademlia/RepublishLeaseSetJob.java
+++ router/java/src/net/i2p/router/networkdb/kademlia/RepublishLeaseSetJob.java
@@ -22,9 +22,7 @@ import net.i2p.util.Log;
  */
 class RepublishLeaseSetJob extends JobImpl {
     private final Log _log;
-//    public final static long REPUBLISH_LEASESET_TIMEOUT = 60*1000;
-    public final static long REPUBLISH_LEASESET_TIMEOUT = 2*60*1000;
-//    private final static int RETRY_DELAY = 20*1000;
+    public final static long REPUBLISH_LEASESET_TIMEOUT = 3*60*1000;
     private final static int RETRY_DELAY = 15*1000;
     private final Hash _dest;
     private final KademliaNetworkDatabaseFacade _facade;
@@ -60,7 +58,7 @@ class RepublishLeaseSetJob extends JobImpl {
                     }
                 } else {
                     if (_log.shouldWarn())
-                        _log.warn("Client [" + _dest.toBase32().substring(0,6) + "] is local, but no valid LeaseSet found; being rebuilt?");
+                        _log.warn("Client [" + _dest.toBase32().substring(0,6) + "] is local, but no valid LeaseSet found -> Being rebuilt?");
                 }
                 return;
             } else {
