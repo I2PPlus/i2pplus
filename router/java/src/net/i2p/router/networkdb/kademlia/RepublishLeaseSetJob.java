23dfb40563551464da9e2c7d698dc2cf914f33fc
dr|z3d <z3d@i2pmail.org>
2024-08-21 00:56:03 +0000

    Router: reduce leaseset republish timeout, let FDNF pick up the slack

--- router/java/src/net/i2p/router/networkdb/kademlia/RepublishLeaseSetJob.java
+++ router/java/src/net/i2p/router/networkdb/kademlia/RepublishLeaseSetJob.java
@@ -16,7 +16,7 @@ import net.i2p.util.Log;
  */
 class RepublishLeaseSetJob extends JobImpl {
     private final Log _log;
-    public final static long REPUBLISH_LEASESET_TIMEOUT = 3 * 60 * 1000;
+    public final static long REPUBLISH_LEASESET_TIMEOUT = 90 * 1000;
     private final static int RETRY_DELAY = 5 * 1000;
     private final Hash _dest;
     private final KademliaNetworkDatabaseFacade _facade;
