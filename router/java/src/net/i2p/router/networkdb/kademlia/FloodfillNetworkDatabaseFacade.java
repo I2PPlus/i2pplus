48071fd1a539e465579117cebb9b0c6c020acce4
dr|z3d <z3d@i2pmail.org>
2024-09-18 08:28:05 +0000

    Router: increase Max_TO_FLOOD in FNDF

--- router/java/src/net/i2p/router/networkdb/kademlia/FloodfillNetworkDatabaseFacade.java
+++ router/java/src/net/i2p/router/networkdb/kademlia/FloodfillNetworkDatabaseFacade.java
@@ -67,7 +67,7 @@ public class FloodfillNetworkDatabaseFacade extends KademliaNetworkDatabaseFacad
      *  4 as of 0.9.2; 3 as of 0.9.9
      */
 
-    public static final int MAX_TO_FLOOD = SystemVersion.isSlow() ? 4 : 8;
+    public static final int MAX_TO_FLOOD = SystemVersion.isSlow() ? 6 : 12;
     private static final int FLOOD_PRIORITY = OutNetMessage.PRIORITY_NETDB_FLOOD;
     private static final int FLOOD_TIMEOUT = 90*1000;
     static final long NEXT_RKEY_RI_ADVANCE_TIME = 45*60*1000;
