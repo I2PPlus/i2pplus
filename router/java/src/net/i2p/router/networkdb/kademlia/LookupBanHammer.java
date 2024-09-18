be85463e0c677462337c0e43a4ac1acf4298c598
dr|z3d <z3d@i2pmail.org>
2024-09-18 08:29:30 +0000

    Router: increase max lookups to use when we're FF before short temp ban; logging tweaks

--- router/java/src/net/i2p/router/networkdb/kademlia/LookupBanHammer.java
+++ router/java/src/net/i2p/router/networkdb/kademlia/LookupBanHammer.java
@@ -19,7 +19,7 @@ class LookupBanHammer {
     private final ObjectCounter<ReplyTunnel> counter;
     /** the id of this is -1 */
     private static final TunnelId DUMMY_ID = new TunnelId();
-    private static final int MAX_LOOKUPS = 30;
+    private static final int MAX_LOOKUPS = 90;
     private static final long CLEAN_TIME = 30*1000;
 
     LookupBanHammer() {
