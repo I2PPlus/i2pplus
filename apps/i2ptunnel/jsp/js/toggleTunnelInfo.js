e1fc8807327c0a3f24fda701b09e93bf7ba424e1
dr|z3d <z3d@i2pmail.org>
2024-11-03 05:51:10 +0000

    bump to -5+

--- apps/i2ptunnel/jsp/js/toggleTunnelInfo.js
+++ apps/i2ptunnel/jsp/js/toggleTunnelInfo.js
@@ -5,12 +5,12 @@ function initToggleInfo() {
   for (i = 0; i < x.length; i++) {
     if (x[i].style.display === "none") {
       x[i].style.display = "table-row";
-      toggle.innerHTML = "<img src='/themes/console/dark/images/collapse_hover.svg' title='Hide Tunnel Info'>";
-      toggle.classList.add('collapse');
+      toggle.innerHTML = '<img src=/themes/console/dark/images/collapse_hover.svg title="Hide Tunnel Info">';
+      toggle.classList.add("collapse");
     } else {
       x[i].style.display = "none";
-      toggle.innerHTML = "<img src='/themes/console/dark/images/expand_hover.svg' title='Show Tunnel Info'>";
-      toggle.classList.remove('collapse');
+      toggle.innerHTML = '<img src=/themes/console/dark/images/expand_hover.svg title="Show Tunnel Info">';
+      toggle.classList.remove("collapse");
     }
   }
 }
