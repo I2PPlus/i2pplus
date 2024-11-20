196c146d3f5ce411fe816af859398e89d02f072a
dr|z3d <z3d@i2pmail.org>
2024-11-20 23:15:14 +0000

    Console: optimize and condense configclients.js

--- apps/routerconsole/jsp/js/configclients.js
+++ apps/routerconsole/jsp/js/configclients.js
@@ -1,20 +1,10 @@
-function init() {
-	var buttons = document.getElementsByClassName("delete");
-	for(index = 0; index < buttons.length; index++) {
-		var button = buttons[index];
-		addClickHandler(button);
-	}
-}
-
-function addClickHandler(elem) {
-	elem.addEventListener("click", function() {
-		if (!confirm(deleteMessage.replace("{0}", elem.getAttribute("client")))) {
-			event.preventDefault();
-			return false;
-		}
-	});
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-	init();
-}, true);
+(() => {
+  document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".control.delete").forEach(btn => {
+      btn.addEventListener("click", event => {
+        const client = btn.getAttribute("client");
+        if (client && !confirm(deleteMessage.replace("{0}", client))) event.preventDefault();
+      });
+    });
+  });
+})();
