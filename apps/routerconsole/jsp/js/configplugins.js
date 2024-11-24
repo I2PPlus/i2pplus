50d139e13900e5e34cd875d2e1cf803b91ad0cf4
dr|z3d <z3d@i2pmail.org>
2024-11-24 21:11:50 +0000

    Console: null check in configplugins.js

--- apps/routerconsole/jsp/js/configplugins.js
+++ apps/routerconsole/jsp/js/configplugins.js
@@ -4,6 +4,7 @@
   document.addEventListener("DOMContentLoaded", () => {
     let total = 0, started = 0, stopped = 0;
     const header = document.getElementById("pconfig");
+    if (!header) return;
     const controlElements = document.querySelectorAll("#pluginconfig td .control.accept, #pluginconfig td .control.stop");
 
     controlElements.forEach(element => {
