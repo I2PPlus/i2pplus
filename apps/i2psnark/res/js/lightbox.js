bd8b1373ff795d5502f9754b604ae04c4c843108
dr|z3d <z3d@i2pmail.org>
2024-11-28 10:09:22 +0000

    I2PSnark: remove getImgDimensions function from lightbox.js

--- apps/i2psnark/res/js/lightbox.js
+++ apps/i2psnark/res/js/lightbox.js
@@ -380,15 +380,4 @@ class Lightbox {
   }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  [...document.querySelectorAll(".thumb")].forEach((thumb, i) => {
-    const span = document.createElement("span");
-    span.classList.add("dimensions");
-    span.textContent = `${thumb.naturalWidth}x${thumb.naturalHeight}`;
-    const fileNameTd = document.querySelectorAll(".snarkFileName")[i];
-    const link = fileNameTd.querySelector("a");
-    fileNameTd.insertBefore(span, link.nextSibling);
-  });
-});
-
 export { Lightbox };
