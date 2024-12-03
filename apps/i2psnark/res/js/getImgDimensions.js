990bc27da9613aa6627e7904b5f9e00e38b03ce1
dr|z3d <z3d@i2pmail.org>
2024-12-03 18:34:35 +0000

    I2PSnark: Add license header to getImgDimensions.js

--- apps/i2psnark/res/js/getImgDimensions.js
+++ apps/i2psnark/res/js/getImgDimensions.js
@@ -1,19 +1,19 @@
+/* I2P+ getImgDimensions.js for I2PSnark by dr|z3d */
+/* Display image dimensions in Snark's file viewer */
+/* License: AGPL3 or later */
+
 document.addEventListener("DOMContentLoaded", () => {
   [...document.querySelectorAll(".thumb")].forEach(thumb => {
     const span = document.createElement("span");
     span.classList.add("dimensions");
     span.textContent = `${thumb.naturalWidth}x${thumb.naturalHeight}`;
-    const fileIconTd = thumb.closest('.fileIcon');
+    const fileIconTd = thumb.closest(".fileIcon");
     if (fileIconTd) {
-      const parentRow = fileIconTd.closest('tr');
-      const snarkFileNameTd = Array.from(parentRow.cells).find(cell =>
-        cell.classList.contains('snarkFileName')
-      );
+      const parentRow = fileIconTd.closest("tr");
+      const snarkFileNameTd = Array.from(parentRow.cells).find(cell => cell.classList.contains("snarkFileName"));
       if (snarkFileNameTd) {
         const link = snarkFileNameTd.querySelector("a");
-        if (link) {
-          snarkFileNameTd.insertBefore(span, link.nextSibling);
-        }
+        if (link) {snarkFileNameTd.insertBefore(span, link.nextSibling);}
       }
     }
   });
