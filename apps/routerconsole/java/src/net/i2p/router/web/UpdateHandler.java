ebdec2edbbbb56bbbbeba32f3541907c1b8018d7
dr|z3d <z3d@i2pmail.org>
2025-01-08 00:57:23 +0000

    Console: don't enforce nonce for sidebar updates

--- apps/routerconsole/java/src/net/i2p/router/web/UpdateHandler.java
+++ apps/routerconsole/java/src/net/i2p/router/web/UpdateHandler.java
@@ -11,13 +11,11 @@ import net.i2p.util.Log;
 /**
  * <p>Handles the request to update the router by firing one or more
  * {@link net.i2p.util.EepGet} calls to download the latest signed update file
- * and displaying the status to anyone who asks.
- * </p>
+ * and displaying the status to anyone who asks.</p>
  * <p>After the download completes the signed update file is verified with
  * {@link net.i2p.crypto.TrustedUpdate}, and if it's authentic the payload
  * of the signed update file is unpacked and the router is restarted to complete
- * the update process.
- * </p>
+ * the update process.</p>
  *
  * This is like a FormHandler but we don't extend it, as we don't have the message area, etc.
  */
@@ -27,9 +25,7 @@ public class UpdateHandler {
     private String _action;
     private String _nonce;
 
-    public UpdateHandler() {
-        this(ContextHelper.getContext(null));
-    }
+    public UpdateHandler() {this(ContextHelper.getContext(null));}
 
     public UpdateHandler(RouterContext ctx) {
         _context = ctx;
@@ -42,8 +38,7 @@ public class UpdateHandler {
      */
     public static ConsoleUpdateManager updateManager(RouterContext ctx) {
         ClientAppManager cmgr = ctx.clientAppManager();
-        if (cmgr == null)
-            return null;
+        if (cmgr == null) {return null;}
         return (ConsoleUpdateManager) cmgr.getRegisteredApp(UpdateManager.APP_NAME);
     }
 
@@ -57,12 +52,10 @@ public class UpdateHandler {
         try {
             _context = ContextHelper.getContext(contextId);
             _log = _context.logManager().getLog(UpdateHandler.class);
-        } catch (Throwable t) {
-            t.printStackTrace();
-        }
+        } catch (Throwable t) {t.printStackTrace();}
     }
 
-    /** these two can be set in either order, so call checkUpdateAction() twice */
+    /** These two can be set in either order, so call checkUpdateAction() twice */
     public void setUpdateAction(String val) {
         _action = val;
         checkUpdateAction();
@@ -74,26 +67,15 @@ public class UpdateHandler {
     }
 
     private void checkUpdateAction() {
-        if (_nonce == null || _action == null) return;
-        if (_nonce.equals(System.getProperty("net.i2p.router.web.UpdateHandler.nonce")) ||
-            _nonce.equals(System.getProperty("net.i2p.router.web.UpdateHandler.noncePrev"))) {
-            if (_action.contains("Unsigned")) {
-                update(ROUTER_UNSIGNED);
-            } else if (_action.contains("DevSU3")) {
-                update(ROUTER_DEV_SU3);
-            } else if (ConfigUpdateHandler.USE_SU3_UPDATE) {
-                update(ROUTER_SIGNED_SU3);
-            } else {
-                // disabled, shouldn't get here
-                //update(ROUTER_SIGNED);
-            }
-        }
+        if (_action == null) {return;}
+        else if (_action.contains("Unsigned")) {update(ROUTER_UNSIGNED);}
+        else if (_action.contains("DevSU3")) {update(ROUTER_DEV_SU3);}
+        else if (ConfigUpdateHandler.USE_SU3_UPDATE) {update(ROUTER_SIGNED_SU3);}
     }
 
     private void update(UpdateType type) {
         ConsoleUpdateManager mgr = updateManager(_context);
-        if (mgr == null)
-            return;
+        if (mgr == null) {return;}
         if (mgr.isUpdateInProgress(ROUTER_SIGNED) || mgr.isUpdateInProgress(ROUTER_UNSIGNED) ||
             mgr.isUpdateInProgress(ROUTER_SIGNED_SU3) || mgr.isUpdateInProgress(ROUTER_DEV_SU3)) {
             _log.error("Update already running");
@@ -101,4 +83,5 @@ public class UpdateHandler {
         }
         mgr.update(type);
     }
+
 }
