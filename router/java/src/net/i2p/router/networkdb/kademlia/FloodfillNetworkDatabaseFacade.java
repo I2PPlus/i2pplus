b0c1766440573c9e4b8bf62dfbd28e2c3de9b161
dr|z3d <z3d@i2pmail.org>
2025-01-29 00:05:25 +0000

    NetDB: RI Publish improvements part 3
    Only publish non-ff at shutdown if not restarting
    (manual merge of upstream commit 01b4c62f)

--- router/java/src/net/i2p/router/networkdb/kademlia/FloodfillNetworkDatabaseFacade.java
+++ router/java/src/net/i2p/router/networkdb/kademlia/FloodfillNetworkDatabaseFacade.java
@@ -163,9 +163,8 @@ public class FloodfillNetworkDatabaseFacade extends KademliaNetworkDatabaseFacad
     @Override
     public synchronized void shutdown() {
         // only if not forced ff or not restarting
-        if (_floodfillEnabled && (!_context.getBooleanProperty(FloodfillMonitorJob.PROP_FLOODFILL_PARTICIPANT) ||
-            !(_context.router().scheduledGracefulExitCode() == Router.EXIT_HARD_RESTART ||
-            _context.router().scheduledGracefulExitCode() == Router.EXIT_GRACEFUL_RESTART))) {
+        if (_floodfillEnabled && !(_context.router().scheduledGracefulExitCode() == Router.EXIT_HARD_RESTART ||
+            _context.router().scheduledGracefulExitCode() == Router.EXIT_GRACEFUL_RESTART)) {
             // turn off to build a new RI...
             _floodfillEnabled = false;
             // true -> publish inline
