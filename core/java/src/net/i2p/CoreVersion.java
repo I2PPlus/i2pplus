304d444479c7ccc763466b27253420890bd5ba7b
dr|z3d <z3d@i2pmail.org>
2025-02-05 15:30:35 +0000

    0.9.65 / 2.8.0+

--- core/java/src/net/i2p/CoreVersion.java
+++ core/java/src/net/i2p/CoreVersion.java
@@ -21,47 +21,42 @@ public class CoreVersion {
     public final static String ID = "Git";
 
     /**
-     *  The version used when checking for router updates,
-     *  and exchanged between router and client over I2CP.
-     *  This is the marketing and user-visible version.
+     *  The version used when checking for router updates, and exchanged between
+     *  router and client over I2CP. This is the marketing and user-visible version.
      *
-     *  If we ever need a point release for a specific
-     *  architecture only, append ".1" to VERSION
-     *  and leave PUBLISHED_VERSION unchanged.
-     *  Otherwise, the same as PUBLISHED_VERSION.
+     *  If we ever need a point release for a specific architecture only, append ".1" to VERSION
+     *  and leave PUBLISHED_VERSION unchanged. Otherwise, the same as PUBLISHED_VERSION.
+     *
      *  RouterVersion.FULL_VERSION is suggested for display to the user.
      */
     public final static String VERSION = "2.7.0";
 
     /**
-     *  The version published in the netdb via StatisticsManager.
-     *  This is the API version.
-     *  It must not go to 1.x for several years, because through
-     *  0.9.49, the Sybil analyzer blocked releases that didn't
-     *  start with "0.9."
+     *  The version published in the netdb via StatisticsManager. This is the API version.
      *
-     *  If we ever need a point release for a specific
-     *  architecture only, append ".1" to VERSION
-     *  and leave PUBLISHED_VERSION unchanged.
-     *  Otherwise, the same as VERSION.
+     *  It must not go to 1.x for several years, because through 0.9.49, the Sybil analyzer
+     *  blocked releases that didn't start with "0.9."
+     *
+     *  If we ever need a point release for a specific architecture only, append ".1" to VERSION
+     *  and leave PUBLISHED_VERSION unchanged. Otherwise, the same as VERSION.
+     *
      *  RouterVersion.FULL_VERSION is suggested for display to the user.
      *
      *  @since 0.9.46
      */
-    public final static String PUBLISHED_VERSION = "0.9.64";
+    public final static String PUBLISHED_VERSION = "0.9.65";
 
     /**
      *  For Vuze.
      *  @return VERSION
      *  @since 0.9.19
      */
-    public static String getVersion() {
-        return VERSION;
-    }
+    public static String getVersion() {return VERSION;}
 
     public static void main(String args[]) {
         System.out.println("I2P+ Core version: " + VERSION);
         System.out.println("I2P+ API version: " + PUBLISHED_VERSION);
         //System.out.println("ID: " + ID);
     }
+
 }
