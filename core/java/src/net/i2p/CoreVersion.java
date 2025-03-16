dc11858ffd886d61f08437b8a087b511a26998c6
dr|z3d <z3d@i2pmail.org>
2025-03-16 04:54:50 +0000

    2.8.5

--- core/java/src/net/i2p/CoreVersion.java
+++ core/java/src/net/i2p/CoreVersion.java
@@ -29,7 +29,7 @@ public class CoreVersion {
      *
      *  RouterVersion.FULL_VERSION is suggested for display to the user.
      */
-    public final static String VERSION = "2.8.0";
+    public final static String VERSION = "2.8.5";
 
     /**
      *  The version published in the netdb via StatisticsManager. This is the API version.
