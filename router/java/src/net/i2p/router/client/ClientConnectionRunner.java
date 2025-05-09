28e1ef15d63b7fc1f5b903269dfa8eaebc8680b7
dr|z3d <z3d@i2pmail.org>
2025-05-09 21:43:49 +0000

    I2CP: Log IP on authentication failure
    (manual merge of upstream commits 6b2689bb07 and 8df589b142)

--- router/java/src/net/i2p/router/client/ClientConnectionRunner.java
+++ router/java/src/net/i2p/router/client/ClientConnectionRunner.java
@@ -13,6 +13,7 @@ import java.io.BufferedOutputStream;
 import java.io.EOFException;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.ArrayList;
@@ -237,6 +238,13 @@ class ClientConnectionRunner {
             _alreadyProcessed.clear();
         }
         _sessions.clear();
+    }
+
+    /**
+     *  @since 0.9.66
+     */
+    public InetAddress getAddress() {
+        return _socket.getInetAddress();
     }
 
     /**
