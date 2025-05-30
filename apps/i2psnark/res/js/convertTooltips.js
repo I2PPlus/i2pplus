bc9b5f57f594b23f19b4490542dc35029445388c
dr|z3d <z3d@i2pmail.org>
2025-05-30 07:40:18 +0000

    I2PSnark: use a mutationobserver to monitor DOM changes in convertTooltips.js

--- apps/i2psnark/res/js/convertTooltips.js
+++ apps/i2psnark/res/js/convertTooltips.js
@@ -18,14 +18,23 @@
   const styleElement = document.createElement("style");
   styleElement.innerHTML =
     '.barTooltip{position:relative}' +
-    '.barTooltip::before,.barTooltip::after{position:absolute;left:50%;opacity:0;pointer-events:none;transition:ease .2s opacity .2s}' +
-    '.barTooltip::before{padding:5px 10px;position:absolute;bottom:120%;z-index:999;line-height:1.3;white-space:pre;font-size:90%;color:var(--ink);border-radius:4px;background:var(--tooltip);transform:translateX(-50%);content:attr(data-tooltip)}' +
-    '.barTooltip::after{height:0;width:0;bottom:calc(120% - 6px);z-index:998;border:6px solid var(--tooltip);content:"";transform:translateX(-50%) rotate(45deg)}'+
-    '.barTooltip:hover::before,.barTooltip:hover::after,txd:hover .barTooltip::before,.txd:hover .barTooltip::after{opacity:1}' +
-    '.barTooltip:not(:hover)::before,.barTooltip:not(:hover)::after{transition:ease .15s opacity .15s}';
+    '.barTooltip:hover,#snarkTbody:hover .barTooltip{overflow:visible}' +
+    '.barTooltip::before,.barTooltip::after{position:absolute;left:50%;opacity:0;box-shadow:2px 2px 2px #0004;pointer-events:none;transition:ease .2s opacity .2s}' +
+    '.barTooltip::before{padding:5px 10px;position:absolute;bottom:120%;z-index:999;line-height:1.3;white-space:pre;font-size:90%;font-weight:500;color:var(--ink);border-radius:4px;background:var(--tooltip);transform:translateX(-50%);content:attr(data-tooltip)}' +
+    '.barTooltip::after{bottom:calc(120% - 6px);z-index:999;border:6px solid var(--tooltip);content:"";transform:translateX(-50%) rotate(45deg)}'+
+    '.barTooltip:hover::before,.barTooltip:hover::after,txd:hover .barTooltip::before,txd:hover .barTooltip::after{opacity:1}';
   document.head.appendChild(styleElement);
 
   convertTooltip(".tx[title], .barComplete[title]");
 
-  document.body.addEventListener('DOMNodeInserted', () => convertTooltip(".tx[title], .barComplete[title]"));
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      if (mutation.addedNodes.length) {
+        convertTooltip(".tx[title], .barComplete[title]");
+      }
+    });
+  });
+
+  const targetNode = document.querySelector('#torrents tbody');
+  observer.observe(targetNode, { childList: true, subtree: true });
 })();
