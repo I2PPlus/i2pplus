07d8a9447b797e5feb149325963488fff3f5ca7b
dr|z3d <z3d@i2pmail.org>
2025-05-30 15:13:15 +0000

    I2PSnark: convertTooltips.js tweak

--- apps/i2psnark/res/js/convertTooltips.js
+++ apps/i2psnark/res/js/convertTooltips.js
@@ -22,6 +22,8 @@
     '.barTooltip::before,.barTooltip::after{position:absolute;left:50%;opacity:0;box-shadow:2px 2px 2px #0004;pointer-events:none;transition:ease .2s opacity .2s}' +
     '.barTooltip::before{padding:5px 10px;position:absolute;bottom:120%;z-index:999;line-height:1.3;white-space:pre;font-size:90%;font-weight:500;color:var(--ink);border-radius:4px;background:var(--tooltip);transform:translateX(-50%);content:attr(data-tooltip)}' +
     '.barTooltip::after{bottom:calc(120% - 6px);z-index:999;border:6px solid var(--tooltip);content:"";transform:translateX(-50%) rotate(45deg)}'+
+    '.txd .barTooltip::before{bottom:calc(128%)}' +
+    '.txd .barTooltip::after{bottom:calc(128% - 6px)}' +
     '.barTooltip:hover::before,.barTooltip:hover::after,txd:hover .barTooltip::before,txd:hover .barTooltip::after{opacity:1}';
   document.head.appendChild(styleElement);
 
