a4dc77a7586591b399400075dc65815e7075cd7e
vituperative <z3d@i2pmail.org>
2025-07-02 15:28:22 +0000

    I2CP: Don't send multiple SetDate messages to sessions with subsessions (as reported by mareki2p)
    (manual merge of upstream commit 5a1f01d81d)

--- router/java/src/net/i2p/router/client/ClientManager.java
+++ router/java/src/net/i2p/router/client/ClientManager.java
@@ -739,7 +739,11 @@ class ClientManager {
 
         public void timeReached() {
             if (!_isStarted) {return;}
-            for (ClientConnectionRunner runner : _runners.values()) {
+            if (_runners.isEmpty()) {schedule(LOOP_TIME); return;}
+
+            // dedup subsessions
+            Set<ClientConnectionRunner> runners = new HashSet<ClientConnectionRunner>(_runners.values());
+            for (ClientConnectionRunner runner : runners) {
                 if (runner instanceof QueuedClientConnectionRunner) {continue;}
                 if (runner.isDead()) {continue;}
                 SessionConfig cfg = runner.getPrimaryConfig();
@@ -751,8 +755,9 @@ class ClientManager {
                     runner.doSend(new SetDateMessage(runner.getClientVersion() != null ?
                                                      CoreVersion.PUBLISHED_VERSION : null));
                 } catch (I2CPMessageException ime) {}
-                if (_isStarted) {schedule(LOOP_TIME);}
             }
+
+            if (_isStarted) {schedule(LOOP_TIME);}
         }
     }
 
