46ecde3fed08754e5e69e6aac83c0d1d088d7ad8
dr|z3d <z3d@i2pmail.org>
2025-09-12 05:14:32 +0000

    Console: more refinements to stickySidebar.js

--- apps/routerconsole/jsp/js/stickySidebar.js
+++ apps/routerconsole/jsp/js/stickySidebar.js
@@ -1,48 +1,42 @@
 /* I2P+ StickySidebar by dr|z3d */
 /* License: AGPLv3 or later */
-
 function stickySidebar() {
-  const html = document.querySelector("html");
-  const htmlHeight = html.getBoundingClientRect().height;
-  const sb = document.getElementById("sidebar");
-  const sbHeight = sb.getBoundingClientRect().height;
   const sbWrap = document.getElementById("sb_wrap");
-  const viewportHeight = window.visualViewport.height;
-  const viewportWidth = window.visualViewport.width;
-  const sectionToggle = document.querySelector("toggleSection");
+  const sb = document.getElementById("sidebar");
   const iframe = document.querySelector(".embed");
-  let debug = false;
-
-  if (!sbWrap) {return;}
+  if (!sbWrap || !sb) return;
 
   function calcHeight() {
-    if ((sbHeight + 5 < viewportHeight && (htmlHeight > viewportHeight) && viewportHeight > 700) ||
-        (iframe !== null && iframe.getBoundingClientRect().height > viewportHeight && viewportHeight > 700)) {
+    const htmlHeight = document.documentElement.getBoundingClientRect().height;
+    const sbHeight = sb.getBoundingClientRect().height;
+    const viewportHeight = window.visualViewport ? window.visualViewport.height : window.innerHeight;
+
+    if (((sbHeight + 5 < viewportHeight) && (htmlHeight > viewportHeight) && (viewportHeight > 700)) ||
+        (iframe && iframe.getBoundingClientRect().height > viewportHeight && viewportHeight > 700)) {
       sbWrap.style.position = "sticky";
       sbWrap.style.top = "5px";
       sbWrap.classList.add("sticky");
     } else {
-      sbWrap.style.position = null;
-      sbWrap.style.top = null;
+      sbWrap.style.position = "";
+      sbWrap.style.top = "";
       sbWrap.classList.remove("sticky");
     }
-    if (debug) {
-      if (iframe !== null && iframe.getBoundingClientRect().height > viewportHeight) {
-        console.log("Iframe height currently reported as:" + iframe.getBoundingClientRect().height);
-      }
-    }
   }
-  calcHeight();
 
-  if (iframe) { setTimeout(() => { calcHeight(); }, 1000); }
-  else {calcHeight();}
+  if (iframe) {
+    sbWrap.classList.remove("sticky");
+    setTimeout(() => { calcHeight(); }, 1500);
+  } else {
+    calcHeight();
+  }
 
-  sbWrap.addEventListener("click", function(element) {
-    if (element.target.className === "toggleSection") {calcHeight();}
+  sbWrap.addEventListener("click", function(event) {
+    if (event.target.classList.contains("toggleSection")) {
+      setTimeout(() => { calcHeight(); }, 500);
+    }
   });
 
-  window.addEventListener("resize", () => { calcHeight(); });
-
+  window.addEventListener("resize", calcHeight);
 }
 
-export {stickySidebar};
+export { stickySidebar };
