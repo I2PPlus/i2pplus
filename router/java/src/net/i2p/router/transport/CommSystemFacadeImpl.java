b9deccca6c8bfd30c466b8af0c328fb8c366111d
dr|z3d <z3d@i2pmail.org>
2025-09-19 05:58:35 +0000

    Router: syntax fixup

--- router/java/src/net/i2p/router/transport/CommSystemFacadeImpl.java
+++ router/java/src/net/i2p/router/transport/CommSystemFacadeImpl.java
@@ -559,7 +559,7 @@ public class CommSystemFacadeImpl extends CommSystemFacade {
      */
     private static final String RDNS_CACHE_FILE = I2PAppContext.getGlobalContext().getConfigDir() +
                                                   File.separator + "rdnscache.txt";
-    private static final int RDNS_WRITE_INTERVAL 15 * 60 * 1000 + 30;
+    private static final int RDNS_WRITE_INTERVAL = 15 * 60 * 1000 + 30;
     private static final boolean has512MB = SystemVersion.getMaxMemory() >= 512 * 1024 * 1024;
     private static final long EXPIRE_TIME = (!has512MB ? 1L : 2L) * 24 * 60 * 60 * 1000; // 24/48 hours expiration
     private static final long EVICT_THRESHOLD = 3L * 24 * 60 * 60 * 1000; // 3 days expiration for file eviction
