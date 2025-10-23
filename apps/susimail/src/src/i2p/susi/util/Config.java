5f5ae4cc01fe0c4f53b5b88bbfb120ed47c00e89
dr|z3d <z3d@i2pmail.org>
2025-10-23 00:23:33 +0000

    SusiMail: cleanup Config.java

--- apps/susimail/src/src/i2p/susi/util/Config.java
+++ apps/susimail/src/src/i2p/susi/util/Config.java
@@ -1,26 +1,11 @@
 /*
- * Created on Nov 15, 2004
- * 
- *  This file is part of susimail project, see http://susi.i2p/
- *  
- *  Copyright (C) 2004-2005  <susi23@mail.i2p>
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *  
+ * This file is part of SusiMail project for I2P
+ * Created on 05.11.2004
  * $Revision: 1.4 $
+ * Copyright (C) 2004-2005 <susi23@mail.i2p>
+ * License: GPL2 or later
  */
+
 package i2p.susi.util;
 
 import java.io.File;
@@ -28,7 +13,6 @@ import java.io.InputStream;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Properties;
-
 import net.i2p.I2PAppContext;
 import net.i2p.data.DataHelper;
 import net.i2p.util.Log;
@@ -40,171 +24,145 @@ import net.i2p.util.OrderedProperties;
  * @author susi
  */
 public class Config {
-	
-	private static Properties properties, config;
-	private static String configPrefix;
+    private static Properties properties, config;
+    private static String configPrefix;
 
-	/**
-	 * 
-	 * @param name
-	 */
-	public synchronized static String getProperty( String name )
-	{
-		if( configPrefix != null )
-			name = configPrefix + name;
-		
-		String result = null;
-		
-		if( properties == null ) {
-			reloadConfiguration();
-		}
-		
-		result = System.getProperty( name );
-		
-		if( result != null )
-			return result;
-		
-		if( config != null ) {
-			result = config.getProperty( name );
-			if( result != null )
-				return result;
-		}
-		
-		result = properties.getProperty( name );
+    public synchronized static String getProperty(String name) {
+        if (configPrefix != null)
+            name = configPrefix + name;
+        String result = null;
+        if (properties == null) {
+            reloadConfiguration();
+        }
+        result = System.getProperty(name);
+        if (result != null)
+            return result;
+        if (config != null) {
+            result = config.getProperty(name);
+            if (result != null)
+                return result;
+        }
+        result = properties.getProperty(name);
+        return result;
+    }
 
-		return result;
-	}
+    /**
+     *  Don't bother showing a reload config button if this returns false.
+     *  @since 0.9.13
+     */
+    public synchronized static boolean hasConfigFile() {
+        File cfg = new File(I2PAppContext.getGlobalContext().getConfigDir(), "susimail.config");
+        return cfg.exists();
+    }
 
-	/**
-	 *  Don't bother showing a reload config button if this returns false.
-	 *  @since 0.9.13
-	 */
-	public synchronized static boolean hasConfigFile() {
-		File cfg = new File(I2PAppContext.getGlobalContext().getConfigDir(), "susimail.config");
-		return cfg.exists();
-	}
+    public synchronized static void reloadConfiguration() {
+        I2PAppContext ctx = I2PAppContext.getGlobalContext();
+        properties = new Properties();
+        InputStream iv = null;
+        try {
+            iv = Config.class.getResourceAsStream("/susimail.properties");
+            properties.load(iv);
+        } catch (IOException e) {
+            Log log = ctx.logManager().getLog(Config.class);
+            log.error("Could not open WEB-INF/classes/susimail.properties (possibly in jar)", e);
+        } finally {
+            if (iv != null) try { iv.close(); } catch(IOException ioe) {}
+        }
+        try {
+            File cfg = new File(I2PAppContext.getGlobalContext().getConfigDir(), "susimail.config");
+            if (cfg.exists()) {
+                config = new OrderedProperties();
+                DataHelper.loadProps(config, cfg);
+            }
+        } catch (IOException e) {
+            Log log = ctx.logManager().getLog(Config.class);
+            log.error("Could not open susimail.config", e);
+        }
+    }
 
-	/**
-	 * 
-	 *
-	 */
-	public synchronized static void reloadConfiguration()
-	{
-		I2PAppContext ctx = I2PAppContext.getGlobalContext();
-		properties = new Properties();
-		InputStream iv = null;
-		try {
-			iv = Config.class.getResourceAsStream("/susimail.properties");
-			properties.load(iv);
-		} catch (IOException e) {
-			Log log = ctx.logManager().getLog(Config.class);
-			log.error("Could not open WEB-INF/classes/susimail.properties (possibly in jar)", e);
-		} finally {
-			if(iv != null) try { iv.close(); } catch(IOException ioe) {}
-		}
-		try {
-			File cfg = new File(I2PAppContext.getGlobalContext().getConfigDir(), "susimail.config");
-			if (cfg.exists()) {
-				config = new OrderedProperties();
-				DataHelper.loadProps(config, cfg);
-			}
-		} catch (IOException e) {
-			Log log = ctx.logManager().getLog(Config.class);
-			log.error("Could not open susimail.config", e);
-		}
-	}
+    /**
+     * Returns the properties, sorted, WITHOUT the prefix
+     *
+     * @since 0.9.13
+     */
+    public synchronized static Properties getProperties() {
+        Properties rv = new OrderedProperties();
+        if (properties != null) {
+            if (configPrefix == null) {
+                rv.putAll(properties);
+            } else {
+                for (Map.Entry<Object, Object> e : properties.entrySet()) {
+                    String k = (String) e.getKey();
+                    if (k.startsWith(configPrefix))
+                        rv.put(k.substring(configPrefix.length()), e.getValue());
+                }
+            }
+        }
+        if (config != null) {
+            if (configPrefix == null) {
+                rv.putAll(config);
+            } else {
+                for (Map.Entry<Object, Object> e : config.entrySet()) {
+                    String k = (String) e.getKey();
+                    if (k.startsWith(configPrefix))
+                        rv.put(k.substring(configPrefix.length()), e.getValue());
+                }
+            }
+        }
+        return rv;
+    }
 
-	/**
-	 * Returns the properties, sorted, WITHOUT the prefix
-	 *
-	 * @since 0.9.13
-	 */
-	public synchronized static Properties getProperties() {
-		Properties rv = new OrderedProperties();
-		if (properties != null) {
-			if (configPrefix == null) {
-				rv.putAll(properties);
-			} else {
-				for (Map.Entry<Object, Object> e : properties.entrySet()) {
-					String k = (String) e.getKey();
-					if (k.startsWith(configPrefix))
-						rv.put(k.substring(configPrefix.length()), e.getValue());
-				}
-			}
-		}
-		if (config != null) {
-			if (configPrefix == null) {
-				rv.putAll(config);
-			} else {
-				for (Map.Entry<Object, Object> e : config.entrySet()) {
-					String k = (String) e.getKey();
-					if (k.startsWith(configPrefix))
-						rv.put(k.substring(configPrefix.length()), e.getValue());
-				}
-			}
-		}
-		return rv;
-	}
+    /**
+     * Saves the properties. A property not in newProps will be removed but
+     * will not override the default in the resource.
+     *
+     * @param newProps non-null WITHOUT the prefix
+     * @since 0.9.13
+     */
+    public synchronized static void saveConfiguration(Properties newProps) throws IOException {
+        Properties toSave = new OrderedProperties();
+        for (Map.Entry<Object, Object> e : newProps.entrySet()) {
+            Object k = e.getKey();
+            if (configPrefix != null)
+                k = configPrefix + k;
+            toSave.put(k, e.getValue());
+        }
+        config = toSave;
+        File cfg = new File(I2PAppContext.getGlobalContext().getConfigDir(), "susimail.config");
+        DataHelper.storeProps(toSave, cfg);
+    }
 
-	/**
-	 * Saves the properties. A property not in newProps will be removed but
-	 * will not override the default in the resource.
-	 *
-	 * @param newProps non-null WITHOUT the prefix
-	 * @since 0.9.13
-	 */
-	public synchronized static void saveConfiguration(Properties newProps) throws IOException {
-		Properties toSave = new OrderedProperties();
-		for (Map.Entry<Object, Object> e : newProps.entrySet()) {
-			Object k = e.getKey();
-			if (configPrefix != null)
-				k = configPrefix + k;
-			toSave.put(k, e.getValue());
-		}
-		config = toSave;
-		File cfg = new File(I2PAppContext.getGlobalContext().getConfigDir(), "susimail.config");
-		DataHelper.storeProps(toSave, cfg);
-	}
+    /**
+     * @param name
+     * @param defaultValue
+     */
+    public synchronized static String getProperty(String name, String defaultValue) {
+        String result = getProperty(name);
+        return result != null ? result : defaultValue;
+    }
 
-	/**
-	 * 
-	 * @param name
-	 * @param defaultValue
-	 */
-	public synchronized static String getProperty( String name, String defaultValue )
-	{
-		String result = getProperty( name );
-		return result != null ? result : defaultValue;
-	}
-
-	/**
-	 * 
-	 * @param name
-	 * @param defaultValue
-	 */
-	public synchronized static int getProperty( String name, int defaultValue )
-	{
-		int result = defaultValue;
-		
-		String str = getProperty( name );
-		
-		if( str != null ) {
-			try {
-				result = Integer.parseInt( str );
-			}
-			catch( NumberFormatException nfe ) {
-				result = defaultValue;
-			}
-		}
-		return result;
-	}
-
-	/**
-	 * Static! Not for use by multiple applications!
-	 * @param prefix
-	 */
-	public synchronized static void setPrefix( String prefix )
-	{
-		configPrefix = prefix.endsWith( "." ) ? prefix : prefix + ".";
-	}
+    /**
+     * @param name
+     * @param defaultValue
+     */
+    public synchronized static int getProperty(String name, int defaultValue) {
+        int result = defaultValue;
+        String str = getProperty(name);
+        if (str != null) {
+            try {
+                result = Integer.parseInt(str);
+            }
+            catch(NumberFormatException nfe) {
+                result = defaultValue;
+            }
+        }
+        return result;
+    }
+    /**
+     * Static! Not for use by multiple applications!
+     * @param prefix
+     */
+    public synchronized static void setPrefix(String prefix) {
+        configPrefix = prefix.endsWith(".") ? prefix : prefix + ".";
+    }
 }
