13819a8223f5e860bb145be63682ff0d6489f4b0
dr|z3d <z3d@i2pmail.org>
2025-11-08 10:06:54 +0000

    Router: start tunnel builds sooner for client tunnels near expiry, later for exploratory

--- router/java/src/net/i2p/router/tunnel/pool/TunnelPool.java
+++ router/java/src/net/i2p/router/tunnel/pool/TunnelPool.java
@@ -883,7 +883,7 @@ public class TunnelPool {
                 avg += 30*1000; // two minute safety factor
             */
             final int PANIC_FACTOR = 6; // how many builds to kick off when time gets short
-            avg += (_settings.isExploratory() ? 2*60*1000 : 45*1000); // 2m safety factor
+            avg += (_settings.isExploratory() ? 30*1000 : 90*1000); // 2m safety factor
             long now = _context.clock().now();
 
             int expireSoon = 0;
