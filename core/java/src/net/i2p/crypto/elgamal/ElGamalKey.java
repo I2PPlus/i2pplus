54f7f7bc3f84e0344bc516e26ec7834d642d79a5
dr|z3d <z3d@i2pmail.org>
2025-12-05 03:03:46 +0000

    Add missing class-level javadoc for ElGamalKey interface

--- core/java/src/net/i2p/crypto/elgamal/ElGamalKey.java
+++ core/java/src/net/i2p/crypto/elgamal/ElGamalKey.java
@@ -4,6 +4,10 @@ import javax.crypto.interfaces.DHKey;
 
 import net.i2p.crypto.elgamal.spec.ElGamalParameterSpec;
 
+/**
+ * Base interface for ElGamal cryptographic keys.
+ * Defines common methods for ElGamal public and private keys.
+ */
 public interface ElGamalKey
     extends DHKey
 {
