8f923d4732a0cb7634d9dc347351c5dd5200ba26
dr|z3d <z3d@i2pmail.org>
2025-12-15 04:42:26 +0000

    jfree.svg: condense boilerplate header

--- apps/jrobin/java/src/org/jfree/svg/src/util/GradientPaintKey.java
+++ apps/jrobin/java/src/org/jfree/svg/src/util/GradientPaintKey.java
@@ -6,21 +6,7 @@
  *
  * Project Info:  http://www.jfree.org/jfreesvg/index.html
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * [Oracle and Java are registered trademarks of Oracle and/or its affiliates.
- * Other names may be trademarks of their respective owners.]
+ * Licensed under the GPL version 3 or later.
  *
  * If you do not wish to be bound by the terms of the GPL, an alternative
  * commercial license can be purchased.  For details, please see visit the
