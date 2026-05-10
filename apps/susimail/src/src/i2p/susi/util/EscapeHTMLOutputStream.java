d080f26baeadbde7b522782255e7b8f4b5bf2ab8
dr|z3d <z3d@i2pmail.org>
2026-05-10 03:03:40 +0000

    SusiMail: inline formatting tweaks

--- apps/susimail/src/src/i2p/susi/util/EscapeHTMLOutputStream.java
+++ apps/susimail/src/src/i2p/susi/util/EscapeHTMLOutputStream.java
@@ -7,7 +7,7 @@ import net.i2p.data.DataHelper;
 
 /**
  * OutputStream that escapes HTML characters during streaming.
- * Streaming version of DataHelper.escapeHTML(), also escapes '-' for
+ * Streaming version of DataHelper.escapeHTML(), also escapes '--' for
  * comment safety since '--' is disallowed inside HTML comments.
  *
  * @since 0.9.34
@@ -22,9 +22,12 @@ public class EscapeHTMLOutputStream extends FilterOutputStream {
     private static final byte[] MDASH = DataHelper.getASCII("&#45;");
     private static final byte[] BR = DataHelper.getASCII("<br>\n");
 
+    /** Track state: 0=normal, 1=saw '<', 2=saw '<-', 3=prev was '-' */
+    private int _commentState;
 
     public EscapeHTMLOutputStream(OutputStream out) {
         super(out);
+        _commentState = 0;
     }
 
     @Override
@@ -32,29 +35,59 @@ public class EscapeHTMLOutputStream extends FilterOutputStream {
         switch (val) {
             case '&':
                 out.write(AMP);
+                _commentState = 0;
                 break;
             case '"':
                 out.write(QUOT);
+                _commentState = 0;
                 break;
             case '<':
                 out.write(LT);
+                _commentState = 1;
                 break;
             case '>':
-                out.write(GT);
+                if (_commentState == 3) {
+                    out.write(MDASH);
+                    out.write(GT);
+                } else {
+                    out.write(GT);
+                }
+                _commentState = 0;
                 break;
             case '\'':
                 out.write(APOS);
+                _commentState = 0;
                 break;
             case '-':
-                out.write(MDASH);
+                if (_commentState == 2) {
+                    out.write(MDASH);
+                    _commentState = 0;
+                } else if (_commentState == 1) {
+                    out.write('-');
+                    _commentState = 2;
+                } else if (_commentState == 3) {
+                    out.write('-');
+                    out.write('-');
+                    _commentState = 3;
+                } else {
+                    out.write('-');
+                    _commentState = 3;
+                }
+                break;
+            case '\t':
+                out.write(' ');
+                _commentState = 0;
                 break;
             case '\r':
+                _commentState = 0;
                 break;
             case '\n':
                 out.write(BR);
+                _commentState = 0;
                 break;
             default:
                 out.write(val);
+                _commentState = 0;
         }
     }
 
--- apps/susimail/src/src/i2p/susi/util/EscapeHTMLWriter.java
+++ apps/susimail/src/src/i2p/susi/util/EscapeHTMLWriter.java
@@ -6,7 +6,7 @@ import java.io.Writer;
 
 /**
  * Writer that escapes HTML characters during streaming.
- * Streaming version of DataHelper.escapeHTML(), also escapes '-' for
+ * Streaming version of DataHelper.escapeHTML(), also escapes '--' for
  * comment safety since '--' is disallowed inside HTML comments.
  *
  * @since 0.9.34
@@ -20,48 +20,75 @@ public class EscapeHTMLWriter extends FilterWriter {
     private static final String APOS = "&apos;";
     private static final String MDASH = "&#45;";
     private static final String BR = "<br>\n";
-    private static final String EMSP = "&emsp;";
-    private static final String SP4 = "&nbsp;&nbsp;&nbsp; ";
 
+    /** Track state: 0=normal, 1=saw '<', 2=saw '<-', 3=prev was '-' */
+    private int _commentState;
 
-    public EscapeHTMLWriter(Writer out) {super(out);}
+    public EscapeHTMLWriter(Writer out) {super(out); _commentState = 0;}
 
     @Override
     public void write(int c) throws IOException {
         switch (c) {
             case '&':
                 out.write(AMP);
+                _commentState = 0;
                 break;
             case '"':
                 out.write(QUOT);
+                _commentState = 0;
                 break;
             case '<':
                 out.write(LT);
+                _commentState = 1;
                 break;
             case '>':
-                out.write(GT);
+                if (_commentState == 3) {
+                    // ->  - escape the previous hyphen
+                    out.write(MDASH);
+                    out.write('>');
+                } else {
+                    out.write(GT);
+                }
+                _commentState = 0;
                 break;
             case '\'':
                 out.write(APOS);
+                _commentState = 0;
                 break;
             case '-':
-                out.write(MDASH);
-                break;
-            case ' ':
-                // this should be breaking but non-collapsing
-                out.write(EMSP);
+                if (_commentState == 2) {
+                    // <--  - escape this hyphen
+                    out.write(MDASH);
+                    _commentState = 0;
+                } else if (_commentState == 1) {
+                    // <- - first hyphen after '<'
+                    out.write('-');
+                    _commentState = 2;
+                } else if (_commentState == 3) {
+                    // --  - keep both, next char will determine if we escape
+                    out.write('-');
+                    out.write('-');
+                    _commentState = 3;
+                } else {
+                    // normal -
+                    out.write('-');
+                    _commentState = 3;
+                }
                 break;
             case '\t':
-                // this should be breaking but non-collapsing
-                out.write(SP4);
+                out.write(' ');
+                _commentState = 0;
                 break;
             case '\r':
+                _commentState = 0;
                 break;
             case '\n':
                 out.write(BR);
+                _commentState = 0;
                 break;
             default:
                 out.write(c);
+                _commentState = 0;
         }
     }
 
