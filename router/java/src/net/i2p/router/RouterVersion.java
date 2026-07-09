7becfdf5ac2f06addca33181823adc36007e5f40
dr|z3d <z3d@i2pmail.org>
2026-07-09 22:07:18 +0000

    bump to -14+; Tor blocklist update

--- router/java/src/net/i2p/router/RouterVersion.java
+++ router/java/src/net/i2p/router/RouterVersion.java
@@ -20,7 +20,7 @@ public class RouterVersion {
     public static final String VERSION = CoreVersion.VERSION;
     /** for example: "beta", "alpha", "rc" */
     public static final String STATUS = "";
-    public static final long BUILD = 13;
+    public static final long BUILD = 14;
 
     /** for example "-test" */
     public static final String EXTRA = "+";
