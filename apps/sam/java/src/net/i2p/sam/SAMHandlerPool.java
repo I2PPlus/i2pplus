09e797f1ca2903b35ed2e8124a1633b366fdd4bd
dr|z3d <z3d@i2pmail.org>
2026-07-15 17:40:05 +0000

    SAM: Cap pool max 8->3, 1 core + 2 burst sufficient for control-plane

--- apps/sam/java/src/net/i2p/sam/SAMHandlerPool.java
+++ apps/sam/java/src/net/i2p/sam/SAMHandlerPool.java
@@ -90,7 +90,7 @@ class SAMHandlerPool {
             throw new RuntimeException("Cannot open selector", ioe);
         }
         _workers = new ThreadPoolExecutor(
-            1, 8,
+            1, 3,
             15, TimeUnit.SECONDS,
             new ArrayBlockingQueue<Runnable>(8),
             new NamedThreadFactory(),
