165cf3f5ea1effee318534b462475a91b75ce4cb
dr|z3d <z3d@i2pmail.org>
2026-07-15 14:05:42 +0000

    bump to 18+ (git ci testing)

--- router/java/src/net/i2p/router/RouterVersion.java
+++ router/java/src/net/i2p/router/RouterVersion.java
@@ -20,7 +20,7 @@ public class RouterVersion {
     public static final String VERSION = CoreVersion.VERSION;
     /** for example: "beta", "alpha", "rc" */
     public static final String STATUS = "";
-    public static final long BUILD = 17;
+    public static final long BUILD = 18;
 
     /** for example "-test" */
     public static final String EXTRA = "+";
