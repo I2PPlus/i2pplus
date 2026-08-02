153b33ef22ca074e5a6d2d415ceee505cf42f763
dr|z3d <z3d@i2pmail.org>
2026-08-02 02:01:56 +0000

    console: use netdb router count instead of full datastore scan in nav render

--- apps/routerconsole/java/src/net/i2p/router/web/helpers/NetDbHelper.java
+++ apps/routerconsole/java/src/net/i2p/router/web/helpers/NetDbHelper.java
@@ -523,7 +523,7 @@ public class NetDbHelper extends FormHandler {
             if (i == 9 && tab != 9) {continue;} // can't nav to lookup
             if (i == 4) {continue;} // show compact mode for all routers
             if (i == 5 || i == 6) {continue;} // hide standard Leasesets tab in normal/adv. mode,
-            if (i == 10 || i == 11 || _context.netDb().getRouters().isEmpty()) {continue;}
+            if (i == 10 || i == 11 || _context.netDb().getKnownRouters() == 0) {continue;}
             if (i == tab || (i == 3 && tab == 4) || (i == 7 && (tab == 5 || tab == 6))) {buf.append("<span class=tab2>").append(_t(titles[i]));} // we are there
             else { // we are not there, make a link
                 buf.append("<span class=tab>").append("<a href=\"netdb")
