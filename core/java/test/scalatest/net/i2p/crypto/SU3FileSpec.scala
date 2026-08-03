205c4638759692afac80ddabec832286782291c9
dr|z3d <z3d@i2pmail.org>
2026-08-03 23:12:01 +0000

    Tests: self-contained SU3File signature verification spec
    Replaces the vacuous verifySigCLI call and dead meeh/zzz cert fixtures
    with generated keypair + self-signed cert + signed SU3 file created
    in-test. Covers plain verify(), header getter followed by verify(),
    and rejection of tampered content. Fails on pre-fix SU3File (skip()
    digest bypass and _versionLength shadowing).

--- core/java/test/scalatest/net/i2p/crypto/SU3FileSpec.scala
+++ core/java/test/scalatest/net/i2p/crypto/SU3FileSpec.scala
@@ -1,6 +1,8 @@
 package net.i2p.crypto
 
 import java.io.File
+import java.nio.file.Files
+import java.nio.charset.StandardCharsets
 
 import org.scalatest.FunSpec
 import org.scalatest.Matchers
@@ -8,23 +10,51 @@ import org.scalatest.Matchers
 
 class SU3FileSpec extends FunSpec with Matchers {
 
-  def cheater(methodName : String, parameters: (AnyRef,Class[_])*): AnyRef = {
-    val parameterValues = parameters.map(_._1)
-    val parameterTypes = parameters.map(_._2)
-    val method = classOf[SU3File].getDeclaredMethod(methodName, parameterTypes:_*)
-    method.setAccessible(true)
-    method.invoke(classOf[SU3File], parameterValues:_*)
+  private def setCert(su3: SU3File, certFile: File): Unit = {
+    val setCert = classOf[SU3File].getDeclaredMethod("setPublicKeyCertificate", classOf[File])
+    setCert.setAccessible(true)
+    setCert.invoke(su3, certFile)
+  }
+
+  private def makeFixture(): (File, File) = {
+    val dir = Files.createTempDirectory("su3test").toFile
+    dir.deleteOnExit
+    val contentFile = new File(dir, "content.xml")
+    Files.write(contentFile.toPath, "<news>test</news>".getBytes(StandardCharsets.UTF_8))
+    val certFile = new File(dir, "key.crt")
+    val su3File = new File(dir, "news.su3")
+    su3File.deleteOnExit
+    val rv = SelfSignedGenerator.generate("test@mail.i2p", null, null, null, null, null, 365, SigType.ECDSA_SHA256_P256)
+    CertUtil.saveCert(rv(2).asInstanceOf[java.security.cert.X509Certificate], certFile)
+    new SU3File(su3File).write(contentFile, SU3File.TYPE_XML, SU3File.CONTENT_NEWS, "1.0.0", "test@mail.i2p",
+                               rv(1).asInstanceOf[java.security.PrivateKey], SigType.ECDSA_SHA256_P256)
+    (su3File, certFile)
   }
 
   describe("SU3File") {
-    val certFileUrl = getClass.getResource("/meeh_at_mail.i2p.crt")
-    val certFile = new File(certFileUrl.toURI)
+    it("should verify a self-signed file") {
+      val (su3File, certFile) = makeFixture()
+      val su3 = new SU3File(su3File)
+      setCert(su3, certFile)
+      su3.verify() should be (true)
+    }
 
-    val seedFileUrl = getClass.getResource("/i2pseeds.su3")
-    val seedFile = new File(seedFileUrl.toURI)
+    it("should verify after a header getter ran first") {
+      val (su3File, certFile) = makeFixture()
+      val su3 = new SU3File(su3File)
+      setCert(su3, certFile)
+      su3.getSignerString() should be ("test@mail.i2p")
+      su3.verify() should be (true)
+    }
 
-    it("should be able to verify a valid file") {
-      cheater("verifySigCLI", (seedFile.getAbsolutePath, classOf[String]), (certFile.getAbsolutePath, classOf[String]))
+    it("should reject a file with modified content") {
+      val (su3File, certFile) = makeFixture()
+      val data = Files.readAllBytes(su3File.toPath)
+      data(data.length - 5) = (data(data.length - 5) ^ 0xFF).toByte
+      Files.write(su3File.toPath, data)
+      val su3 = new SU3File(su3File)
+      setCert(su3, certFile)
+      su3.verify() should be (false)
     }
   }
 }
