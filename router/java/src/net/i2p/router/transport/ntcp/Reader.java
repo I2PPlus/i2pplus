e2d5239caa18dbbfc0f427f5481836eae606b758
dr|z3d <z3d@i2pmail.org>
2026-08-04 20:06:58 +0000

    router: use notifyAll() instead of notify() in Reader

--- router/java/src/net/i2p/router/transport/ntcp/Reader.java
+++ router/java/src/net/i2p/router/transport/ntcp/Reader.java
@@ -165,7 +165,7 @@ public class Reader {
         synchronized (_pendingConnections) {
             _readAfterLive.remove(con);
             _pendingConnections.remove(con);
-            _pendingConnections.notify(); // necessary?
+            _pendingConnections.notifyAll(); // necessary?
         }
     }
 
