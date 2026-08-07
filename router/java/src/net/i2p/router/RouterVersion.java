affa736dff6b8e62dba2e4d003cc9a81727194ae
dr|z3d <z3d@i2pmail.org>
2026-08-07 13:15:57 +0000

    bump to -4+; console theme updates

--- router/java/src/net/i2p/router/RouterVersion.java
+++ router/java/src/net/i2p/router/RouterVersion.java
@@ -26,7 +26,7 @@ public class RouterVersion {
     /**
      * BUILD.
      */
-    public static final long BUILD = 3;
+    public static final long BUILD = 4;
 
     /** for example "-test" */
     public static final String EXTRA = "+";
