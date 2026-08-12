i2p/i2p.i2p

Streaming: Reset tweaks
- Send reset on nacks hash verification failure
- Set ports on connection reset packet
- Set NO_ACK on no-connection reset packet
- Combine setFlag() calls
- Log tweaks

apps/streaming/java/src/net/i2p/client/streaming/impl/ConnectionManager.java
@@ -265,10 +265,12 @@ class ConnectionManager {
                                 DataHelper.toLong(g, j << 2, 4, nacks[j]);
                             }
                             Hash ghash = new Hash(g);
-                            _log.warn("Sig passed but hash failed, expected: " + hash.toBase32() + " got: " + ghash.toBase32());
+                            _log.warn("Sig passed but hash failed, sending reset, expected: " + hash.toBase32() +
+                                      " got: " + ghash.toBase32() +
+                                      " from: " + from.calculateHash().toBase32());
                         }
-                        sigOk = false;
-                        break;
+                        _packetHandler.sendResetUnverified(synPacket);
+                        return null;
                     }
                 }
                 if (sigOk && _log.shouldDebug())

I2PPlus/i2pplus

apps/streaming/java/src/net/i2p/client/streaming/impl/ConnectionManager.java
@@ -383,11 +383,12 @@ class ConnectionManager {
                                 DataHelper.toLong(g, j << 2, 4, nacks[j]);
                             }
                             Hash ghash = new Hash(g);
-                            if (_log.shouldWarn())
-                                _log.warn("Signature passed but hash failed \n* Expected: " + hash.toBase32() + "\n* Received: " + ghash.toBase32());
+                            _log.warn("Signature passed but hash failed, sending reset, expected: " + hash.toBase32() +
+                                      " received: " + ghash.toBase32() +
+                                      " from: " + from.calculateHash().toBase32());
                         }
-                        sigOk = false;
-                        break;
+                        _packetHandler.sendResetUnverified(synPacket);
+                        return null;
                     }
                 }
                 if (sigOk && _log.shouldInfo())