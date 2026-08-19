e96d483689eec32a1f4f7a7ec951e7887fb3042e
zzz <zzz@i2pmail.org>
2026-08-19 17:14:11 +0000

    i2psnark: Fail in BDecoder on dup dictionary entry
    Cherry-picked from upstream 7751367b8507e06184c829fd5af930d0485f2ec7

--- apps/i2psnark/java/src/org/klomp/snark/bencode/BDecoder.java
+++ apps/i2psnark/java/src/org/klomp/snark/bencode/BDecoder.java
@@ -308,7 +308,9 @@ public class BDecoder {
                 }
 
                 BEValue value = bdecode();
-                result.put(key, value);
+                BEValue old = result.put(key, value);
+                if (old != null)
+                    throw new InvalidBEncodingException("Duplicate key " + key + " in dictionary");
 
                 // XXX ugly hack continued
                 if (special) in_special_map = false;
